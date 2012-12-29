802adac71b9a18be3a8fe55f844dc35574aeee7b
Scott Albertson <scott@thoughtbot.com>
2012-12-28 16:29:44 -0800

    Set up Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Houndapp::Application.config.secret_token = '961ad4ee1c38df9b163da5f2ddf528908387509893e59b1571c65d8e716eb3b152de2a2c7d6a9d253142b4c1759f1f5b2fd321efb4d4caeee7b8ef7dda74706f'
