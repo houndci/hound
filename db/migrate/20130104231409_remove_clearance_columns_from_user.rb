910e0377273384e33e0a8c89fc86fa9f5fbce9d8
Scott Albertson <scott@thoughtbot.com>
2013-01-04 15:20:20 -0800

    Remove Clearance columns from users

---
+++ db/migrate/20130104231409_remove_clearance_columns_from_user.rb
@@ -0,0 +1,21 @@
+class RemoveClearanceColumnsFromUser < ActiveRecord::Migration
+  def up
+    remove_columns(
+      :users, [
+        :email,
+        :encrypted_password,
+        :salt,
+        :confirmation_token,
+        :remember_token
+      ]
+    )
+  end
+
+  def down
+    add_column :users, :email, :string
+    add_column :users, :encrypted_password, :string, limit: 128
+    add_column :users, :salt, :string, limit: 128
+    add_column :users, :confirmation_token, :string, limit: 128
+    add_column :users, :remember_token, :string, limit: 128
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121229180151) do
+ActiveRecord::Schema.define(:version => 20130104231409) do
 
   create_table "users", :force => true do |t|
-    t.string   "email"
-    t.string   "encrypted_password", :limit => 128
-    t.string   "salt",               :limit => 128
-    t.string   "confirmation_token", :limit => 128
-    t.string   "remember_token",     :limit => 128
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
     t.string   "github_username"
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email"
-  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
