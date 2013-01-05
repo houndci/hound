287cd5db1dc130812e2b396ef5cf6b0d285e44b9
Scott Albertson <scott@thoughtbot.com>
2013-01-04 19:13:58 -0800

    Hash style

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_filter :authorize, :only => [:new, :create]
+  skip_before_filter :authorize, only: [:new, :create]
 
   def new
   end
