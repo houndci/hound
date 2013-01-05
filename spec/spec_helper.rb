6b2acffadbb5ba978ad43e0edda2c3c757d81882
Scott Albertson <scott@thoughtbot.com>
2013-01-04 19:08:37 -0800

    Remove capybara/rspec require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'capybara/rspec'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
