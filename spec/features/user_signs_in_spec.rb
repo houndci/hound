986275b8b6ae23434d045e572428b3a768609bdf
Scott Albertson <scott@thoughtbot.com>
2013-01-04 19:14:28 -0800

    Create helper for signing in

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -2,25 +2,24 @@ require 'spec_helper'
 
 feature 'User authentication' do
   scenario 'user signs in successfully', js: true do
-    visit root_path
-
-    click_link 'Sign in'
-    fill_in 'login_field', with: 'jimtom'
-    fill_in 'password', with: '1testing'
-    click_button 'Sign in'
+    sign_in
 
     expect(page).to have_link 'Sign out'
   end
 
   scenario 'user signs out successfully', js: true do
-    visit root_path
+    sign_in
 
+    click_link 'Sign out'
+
+    expect(page).to have_link 'Sign in'
+  end
+
+  def sign_in
+    visit root_path
     click_link 'Sign in'
     fill_in 'login_field', with: 'jimtom'
     fill_in 'password', with: '1testing'
     click_button 'Sign in'
-    click_link 'Sign out'
-
-    expect(page).to have_link 'Sign in'
   end
 end
