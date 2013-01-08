dcd8788689906d5e083eb8e07991086ef27f90fc
Scott Albertson <scott@thoughtbot.com>
2013-01-07 20:41:36 -0800

    Stub GitHub authentication when testing

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -2,24 +2,21 @@ require 'spec_helper'
 
 feature 'User authentication', js: true do
   scenario 'user signs in' do
-    sign_in
+    stub_oauth
+    visit root_path
+
+    click_link 'Sign in'
 
     expect(page).to have_link 'Sign out'
   end
 
   scenario 'user signs out' do
-    sign_in
+    stub_oauth
+    visit root_path
+    click_link 'Sign in'
 
     click_link 'Sign out'
 
     expect(page).to have_link 'Sign in'
-  end
-
-  def sign_in
-    visit root_path
-    click_link 'Sign in'
-    fill_in 'login_field', with: 'jimtom'
-    fill_in 'password', with: '1testing'
-    click_button 'Sign in'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,15 +6,17 @@ require 'rspec/autorun'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
-  # config.mock_with :mocha
-
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
+
+  config.include OauthHelper
 end
 
 Capybara.configure do |config|
   config.javascript_driver = :webkit
-  config.run_server = false
-  config.app_host = 'http://hound.dev:3000'
+end
+
+OmniAuth.configure do |config|
+  config.test_mode = true
 end
