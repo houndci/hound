59a2bf2679b25cad65186193c5789c86419fbc09
Scott Albertson <scott@thoughtbot.com>
2013-01-22 20:35:14 -0800

    Scope repos by current user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ActiveRecord::Base
   attr_accessible :github_username
 
+  has_many :repos
+
   before_create :generate_remember_token
 
   def to_s
     github_username
+  end
+
+  def active_repo_ids_in(repo_ids)
+    active_repos = repos.where(github_id: repo_ids, active: true)
+    active_repos.map(&:github_id)
   end
 
   private
