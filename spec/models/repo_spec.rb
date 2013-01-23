ead49c86c8a6fcb30c4c9b7bad1beee2c4baea41
Scott Albertson <scott@thoughtbot.com>
2013-01-23 14:55:11 -0800

    Do not create unnecessary repo

--- spec/models/repo_spec.rb
+++ spec/models/repo_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe Repo do
-  before do
+  it 'validates uniqueness of github_id' do
     Repo.create(active: false, github_id: 123)
+
+    should validate_uniqueness_of :github_id
   end
 
-  it { should validate_uniqueness_of :github_id }
   it { should validate_presence_of :github_id }
 end
