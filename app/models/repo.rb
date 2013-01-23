8b065c39ef8f58e04e7a23b7de9b3c73831c6142
Scott Albertson <scott@thoughtbot.com>
2013-01-23 14:37:55 -0800

    Use pluck instead of map

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -4,6 +4,6 @@ class Repo < ActiveRecord::Base
   validates :github_id, uniqueness: true, presence: true
 
   def self.active_repo_ids_in(ids)
-    where(github_id: ids, active: true).map(&:github_id)
+    where(github_id: ids, active: true).pluck(:github_id)
   end
 end
