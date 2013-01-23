7ae45184636abf29f0755de280e5d1e1b06c5f8e
Scott Albertson <scott@thoughtbot.com>
2013-01-23 15:01:31 -0800

    Rename helper method to be more clear

--- spec/features/repo_list_spec.rb
+++ spec/features/repo_list_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 feature 'Repo list' do
   scenario 'user views list' do
-    sign_in
+    sign_in_and_stub_repos_request
 
     expect(page).to have_content 'my_private_repo'
   end
 
   scenario 'user activates repo', js: true do
-    sign_in
+    sign_in_and_stub_repos_request
 
     click_link 'on'
 
@@ -16,7 +16,7 @@ feature 'Repo list' do
   end
 
   scenario 'user deactivates repo', js: true do
-    sign_in
+    sign_in_and_stub_repos_request
 
     click_link 'on'
     click_link 'off'
@@ -25,7 +25,7 @@ feature 'Repo list' do
   end
 
   scenario 'user activates repo then refreshes the page', js: true do
-    sign_in
+    sign_in_and_stub_repos_request
 
     click_link 'on'
     reload
@@ -33,7 +33,7 @@ feature 'Repo list' do
     expect(page).to have_link 'off'
   end
 
-  def sign_in
+  def sign_in_and_stub_repos_request
     auth_token = 'authtoken'
     stub_oauth('jimtom', auth_token)
     stub_repos_request(auth_token)
