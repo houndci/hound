c9ec91e8104a462ce7c484138934055e7c6ce603
Scott Albertson <scott@thoughtbot.com>
2013-01-23 10:32:27 -0800

    Specify default for active column

--- db/migrate/20130121230036_add_repos.rb
+++ db/migrate/20130121230036_add_repos.rb
@@ -2,7 +2,7 @@ class AddRepos < ActiveRecord::Migration
   def up
     create_table :repos do |t|
       t.integer :github_id, null: false
-      t.boolean :active, null: false
+      t.boolean :active, null: false, default: false
     end
 
     add_index :repos, :github_id, unique: true
