f30361d558383805837a3772cd717f5bf5798d6f
Scott Albertson <scott@thoughtbot.com>
2013-01-27 12:55:34 -0800

    Extract JavaScript function

--- app/assets/javascripts/repo_list.js
+++ app/assets/javascripts/repo_list.js
@@ -1,47 +1,52 @@
-_.templateSettings = {
-  interpolate : /\{\{(.+?)\}\}/g
-};
-
 RepoList = function(repos) {
+  _.templateSettings = {
+    interpolate : /\{\{(.+?)\}\}/g
+  };
+
+  var template = _.template($('#repo-list-item-template').html());
+
+  function buildListItem(repo) {
+    var repoListItem = $(template(repo));
+    var activateLink = repoListItem.find('a.activate');
+    var deactivateLink = repoListItem.find('a.deactivate');
+    repoListItem.find('a').hide();
+
+    activateLink.click(function(event) {
+      event.preventDefault();
+      $.post('/repo_activations', { github_id: repo.id }, function() {
+        activateLink.hide();
+        deactivateLink.show();
+      });
+    });
+
+    deactivateLink.click(function(event) {
+      event.preventDefault();
+      $.ajax({
+        url: '/repo_activations/' + repo.id,
+        type: 'DELETE',
+        success: function() {
+          deactivateLink.hide();
+          activateLink.show();
+        }
+      });
+    });
+
+    $.get('/repos/' + repo.id, function(data) {
+      if (data.active) {
+        deactivateLink.show();
+      } else {
+        activateLink.show();
+      }
+    });
+
+    return repoListItem;
+  }
+
   return {
     render: function(target) {
-      var template = _.template($('#repo-list-item-template').html());
-
       $.each(repos, function(index, repo) {
-        var repoListItem = $(template(repo));
-        var activateLink = repoListItem.find('a.activate');
-        var deactivateLink = repoListItem.find('a.deactivate');
-        repoListItem.find('a').hide();
-
-        activateLink.click(function(event) {
-          event.preventDefault();
-          $.post('/repo_activations', { github_id: repo.id }, function() {
-            activateLink.hide();
-            deactivateLink.show();
-          });
-        });
-
-        deactivateLink.click(function(event) {
-          event.preventDefault();
-          $.ajax({
-            url: '/repo_activations/' + repo.id,
-            type: 'DELETE',
-            success: function() {
-              deactivateLink.hide();
-              activateLink.show();
-            }
-          });
-        });
-
-        $.get('/repos/' + repo.id, function(data) {
-          if (data.active) {
-            deactivateLink.show();
-          } else {
-            activateLink.show();
-          }
-        });
-
-        target.append(repoListItem);
+        listItem = buildListItem(repo);
+        target.append(listItem);
       });
     }
   };
