3179d07a5e106c7f06d7f908dec7f12da1843f6d
Scott Albertson <scott@thoughtbot.com>
2013-01-28 11:09:19 -0800

    Add method to GithubApi that creates hooks

--- lib/github_api.rb
+++ lib/github_api.rb
@@ -11,4 +11,14 @@ class GithubApi
     client = Octokit::Client.new(oauth_token: token)
     client.repos
   end
+
+  def create_pull_request_hook(full_repo_name, callback_endpoint)
+    client = Octokit::Client.new(oauth_token: token)
+    client.create_hook(
+      full_repo_name,
+      'web',
+      { url: callback_endpoint },
+      { events: ['pull_request'], active: true }
+    )
+  end
 end
--- spec/support/helpers/github_api_helper.rb
+++ spec/support/helpers/github_api_helper.rb
@@ -4,11 +4,25 @@ module GithubApiHelper
       :get,
       'https://api.github.com/user/repos'
     ).with(
-      :headers => { 'Authorization' => "token #{auth_token}" }
+      headers: { 'Authorization' => "token #{auth_token}" }
     ).to_return(
-      :status => 200,
-      :body => File.read('spec/support/fixtures/github_repos_response_for_jimtom.json'),
-      :headers => { 'Content-Type' => 'application/json; charset=utf-8' }
+      status: 200,
+      body: File.read('spec/support/fixtures/github_repos_response_for_jimtom.json'),
+      headers: { 'Content-Type' => 'application/json; charset=utf-8' }
+    )
+  end
+
+  def stub_hook_creation_request(auth_token, full_repo_name, callback_endpoint)
+    stub_request(
+      :post,
+      "https://api.github.com/repos/#{full_repo_name}/hooks"
+    ).with(
+      body: %({"name":"web","config":{"url":"#{callback_endpoint}"},"events":["pull_request"],"active":true}),
+      headers: { 'Authorization' => "token #{auth_token}" }
+    ).to_return(
+      status: 200,
+      body: File.read('spec/support/fixtures/github_hook_creation_response.json'),
+      headers: { 'Content-Type' => 'application/json; charset=utf-8' }
     )
   end
 end
