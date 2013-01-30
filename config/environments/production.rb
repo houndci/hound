07fde9ae58616125b681abf22a3f0c3e55b9198a
Scott Albertson <scott@thoughtbot.com>
2013-01-29 19:52:37 -0800

    Precompile JavaScript in repos directory

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Houndapp::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( repos/*.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
