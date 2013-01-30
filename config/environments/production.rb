36e1f69440349cb784be4aa71dc4eb304eca4fb3
Scott Albertson <scott@thoughtbot.com>
2013-01-29 19:53:00 -0800

    Update default mailer URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Houndapp::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'houndapp.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'houndapp.herokuapp.com' }
 
   # Enable threaded mode
   # config.threadsafe!
