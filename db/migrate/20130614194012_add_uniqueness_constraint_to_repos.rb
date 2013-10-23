4e9cb7825ddf2bb159f8646b48bcf949d12b5e01
Laila Winner <laila@thoughtbot.com>
2013-10-23 15:51:01 -0700

    Make uniqueness constraint migration reversible
    * Add down method to AddUniquenessConstraintToRepos to make migration
      reversible
    * https://trello.com/c/jgd1zB9q

--- db/migrate/20130614194012_add_uniqueness_constraint_to_repos.rb
+++ db/migrate/20130614194012_add_uniqueness_constraint_to_repos.rb
@@ -1,6 +1,11 @@
 class AddUniquenessConstraintToRepos < ActiveRecord::Migration
-  def change
+  def up
     remove_index :repos, :github_id
     add_index :repos, [:user_id, :github_id], unique: true
+  end
+
+  def down
+    remove_index :repos, [:user_id, :github_id]
+    add_index :repos, :github_id
   end
 end
