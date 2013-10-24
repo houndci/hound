041a7e17996de2307cf0fbcebacc3d2d44ec7e4c
Laila Winner <laila@thoughtbot.com>
2013-10-24 16:52:25 -0700

    Show full repo name in repos list
    * https://trello.com/c/DnUbGjI7

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -2,7 +2,7 @@ class ReposController < ApplicationController
   respond_to :json
 
   def index
-    respond_with current_user.repos.order(:name)
+    respond_with current_user.repos.order(:full_github_name)
   end
 
   def update
