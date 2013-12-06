55ec63080d30509e148b78d50a2f39fb239c41bc
Scott Albertson <scott@thoughtbot.com>
2013-12-06 13:21:49 -0800

    Move config to application.rb

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,7 @@ module Houndapp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,4 @@ Houndapp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  config.assets.initialize_on_precompile = false
 end
