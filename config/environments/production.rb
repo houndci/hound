8c6226154c13d6fc62ac9789df9eba4b3f655235
Scott Albertson <scott@thoughtbot.com>
2013-12-06 11:56:39 -0800

    Don't boot Rails on precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Houndapp::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  config.assets.initialize_on_precompile = false
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
