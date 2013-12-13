bd738153f3f98611d1770041d3747380e962082e
Greg Lazarev <greg@thoughtbot.com>
2013-12-12 16:38:48 -0800

    Add some trailing white space

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ActiveRecord::Base
   include ActiveModel::ForbiddenAttributesProtection
 
-  attr_accessible :github_username
+  attr_accessible :github_username  
 
-  has_many :repos
+  has_many :repos   
 
   validates :github_username, presence: true
 
   before_create :generate_remember_token
 
   def to_s
-    github_username
+    github_username   
   end
 
   def github_repo(github_id)
