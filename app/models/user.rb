7793c928d20efc425c9aaab16b712220735bd0d9
Greg Lazarev <greg@thoughtbot.com>
2013-12-13 17:03:09 -0800

    more space

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,21 +8,21 @@ class User < ActiveRecord::Base
 
   before_create :generate_remember_token
 
-  def to_s
-    github_username   
+  def to_s     
+    github_username
   end
 
   def github_repo(github_id)
-    repos.where(github_id: github_id).first     
+    repos.where(github_id: github_id).first
   end
 
-  def create_github_repo(attributes)
-    repos.create(attributes)
+  def create_github_repo(attributes)   
+    repos.create(attributes)      
   end
 
   private
 
-  def generate_remember_token   
+  def generate_remember_token
     self.remember_token = SecureRandom.hex(20)
-  end
+  end    
 end
