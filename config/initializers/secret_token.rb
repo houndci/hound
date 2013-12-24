bdf749a136f221fbf35a6fc5e4d732272cf97c05
Scott Albertson <scott@thoughtbot.com>
2013-12-24 10:23:43 -0800

    Move confiration out of code
    https://trello.com/c/OwROnK5X/80-remove-secret-token-from-codebase

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Houndapp::Application.config.secret_token = '961ad4ee1c38df9b163da5f2ddf528908387509893e59b1571c65d8e716eb3b152de2a2c7d6a9d253142b4c1759f1f5b2fd321efb4d4caeee7b8ef7dda74706f'
+Houndapp::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
