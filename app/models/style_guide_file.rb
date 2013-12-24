d787fddafff13992818802316d865032f31b29db
Scott Albertson <scott@thoughtbot.com>
2013-12-24 13:37:53 -0800

    Add more Rubocop rules
    * Use Rubocop Commissioner to manage cops
    
    https://trello.com/c/Sp4Qke4R/71-hook-up-the-rest-of-rubocop-rules

--- app/models/style_guide_file.rb
+++ app/models/style_guide_file.rb
@@ -1,7 +1,45 @@
 class StyleGuideFile
   RULES = [
+    Rubocop::Cop::Style::AlignArray,
+    Rubocop::Cop::Style::AlignHash,
+    Rubocop::Cop::Style::AlignParameters,
+    Rubocop::Cop::Style::AndOr,
+    Rubocop::Cop::Style::Blocks,
+    Rubocop::Cop::Style::BracesAroundHashParameters,
+    Rubocop::Cop::Style::ClassAndModuleCamelCase,
+    Rubocop::Cop::Style::ClassMethods,
+    Rubocop::Cop::Style::ConstantName,
+    Rubocop::Cop::Style::DefWithParentheses,
+    Rubocop::Cop::Style::DotPosition,
+    Rubocop::Cop::Style::EmptyLineBetweenDefs,
+    Rubocop::Cop::Style::EmptyLines,
+    Rubocop::Cop::Style::EmptyLinesAroundAccessModifier,
+    Rubocop::Cop::Style::EmptyLinesAroundBody,
+    Rubocop::Cop::Style::EndOfLine,
+    Rubocop::Cop::Style::FinalNewline,
+    Rubocop::Cop::Style::HashSyntax,
+    Rubocop::Cop::Style::IndentationWidth,
+    Rubocop::Cop::Style::LineLength,
+    Rubocop::Cop::Style::MethodCallParentheses,
+    Rubocop::Cop::Style::MethodName,
+    Rubocop::Cop::Style::Not,
+    Rubocop::Cop::Style::RedundantBegin,
+    Rubocop::Cop::Style::RedundantReturn,
+    Rubocop::Cop::Style::RedundantSelf,
+    Rubocop::Cop::Style::SpaceAfterControlKeyword,
+    Rubocop::Cop::Style::SpaceAfterMethodName,
+    Rubocop::Cop::Style::SpaceAfterNot,
+    Rubocop::Cop::Style::SpaceAroundBlockBraces,
+    Rubocop::Cop::Style::SpaceAroundEqualsInParameterDefault,
+    Rubocop::Cop::Style::SpaceAroundOperators,
+    Rubocop::Cop::Style::SpaceBeforeModifierKeyword,
+    Rubocop::Cop::Style::SpaceInsideBrackets,
+    Rubocop::Cop::Style::SpaceInsideHashLiteralBraces,
+    Rubocop::Cop::Style::SpaceInsideParens,
+    Rubocop::Cop::Style::StringLiterals,
     Rubocop::Cop::Style::Tab,
-    Rubocop::Cop::Style::TrailingWhitespace
+    Rubocop::Cop::Style::TrailingWhitespace,
+    Rubocop::Cop::Style::UnlessElse
   ]
 
   attr_reader :filename, :contents
@@ -13,27 +51,22 @@ class StyleGuideFile
   end
 
   def violations
-    @violations ||= RULES.map { |rule| violations_for_rule(rule) }.flatten
-  end
-
-  private
-
-  def violations_for_rule(rule)
-    cop = rule.new
-    cop.investigate(source)
-
-    offences = cop.offences.select do |offence|
-      modified_line_number?(offence.line)
+    cops = RULES.map {|rule| rule.new}
+    commissioner = Rubocop::Cop::Commissioner.new(cops)
+    offenses = commissioner.investigate(source).select do |offense|
+      modified_line_number?(offense.line)
     end
 
-    offences.map do |offence|
+    offenses.map do |offense|
       {
-        line_number: offence.line,
-        code: line_of_code(offence.line),
-        message: offence.message
+        line_number: offense.line,
+        code: line_of_code(offense.line),
+        message: offense.message
       }
     end
   end
+
+  private
 
   def line_of_code(line_number)
     source.lines[line_number - 1]
