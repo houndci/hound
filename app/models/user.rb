8ff807bcb3e902ec57c98c2f83205ee64f33bc33
Greg Lazarev <greg@thoughtbot.com>
2013-12-24 18:13:32 -0800

    trailing whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,27 +2,27 @@ class User < ActiveRecord::Base
   include ActiveModel::ForbiddenAttributesProtection
 
   has_many :memberships
-  has_many :repos, through: :memberships    
+  has_many :repos, through: :memberships
 
-  validates :github_username, presence: true   
+  validates :github_username, presence: true
 
   before_create :generate_remember_token
 
-  def to_s     
+  def to_s
     github_username
   end
 
-  def github_repo(github_id)
-    repos.where(github_id: github_id).first
+  def github_repo(github_id)   
+    repos.where(github_id: github_id).first   
   end
 
-  def create_github_repo(attributes)
+  def create_github_repo(attributes) 
     repos.create(attributes)
   end
 
   private
 
   def generate_remember_token
-    self.remember_token = SecureRandom.hex(20)    
+    self.remember_token = SecureRandom.hex(20)
   end
 end
