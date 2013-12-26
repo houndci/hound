ec70d1c15a21c36c6a330f44da0c29f31066b70d
Greg Lazarev <greg@thoughtbot.com>
2013-12-25 22:50:32 -0800

    test trailing whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,24 +5,24 @@ class User < ActiveRecord::Base
   has_many :repos, through: :memberships
 
   validates :github_username, presence: true
-
-  before_create :generate_remember_token
+  
+  before_create :generate_remember_token 
 
   def to_s
-    github_username
+    github_username 
   end
 
-  def github_repo(github_id)   
-    repos.where(github_id: github_id).first   
+  def github_repo(github_id)
+    repos.where(github_id: github_id).first
   end
 
-  def create_github_repo(attributes) 
-    repos.create(attributes)
+  def create_github_repo(attributes)
+    repos.create(attributes)   
   end
 
   private
 
-  def generate_remember_token   
-    self.remember_token = SecureRandom.hex(20)  
+  def generate_remember_token
+    self.remember_token = SecureRandom.hex(20)
   end
 end
