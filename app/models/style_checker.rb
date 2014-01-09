2c2e05554dfbcfbf6f6b87e8cb0e49cdb6b12ec6
Scott Albertson <scott@thoughtbot.com>
2014-01-09 09:53:31 -0800

    Enable appropriate rules
    * Rules were passing because we were using Rubocop incorrectly
    * Enable existing rules and add some more
    * Mild refactor of StyleChecker methods

--- app/models/style_checker.rb
+++ app/models/style_checker.rb
@@ -1,47 +1,55 @@
 class StyleChecker
   FileViolation = Struct.new(:filename, :line_violations)
   LineViolation = Struct.new(:line_number, :code, :messages)
+
   RULES = [
-    Rubocop::Cop::Style::AlignArray,
-    Rubocop::Cop::Style::AlignHash,
-    Rubocop::Cop::Style::AlignParameters,
     Rubocop::Cop::Style::AndOr,
     Rubocop::Cop::Style::Blocks,
+    Rubocop::Cop::Style::BlockComments,
     Rubocop::Cop::Style::BracesAroundHashParameters,
     Rubocop::Cop::Style::ClassAndModuleCamelCase,
     Rubocop::Cop::Style::ClassMethods,
+    Rubocop::Cop::Style::ColonMethodCall,
     Rubocop::Cop::Style::ConstantName,
     Rubocop::Cop::Style::DefWithParentheses,
+    Rubocop::Cop::Style::DefWithoutParentheses,
     Rubocop::Cop::Style::DotPosition,
     Rubocop::Cop::Style::EmptyLineBetweenDefs,
     Rubocop::Cop::Style::EmptyLines,
-    Rubocop::Cop::Style::EmptyLinesAroundAccessModifier,
-    Rubocop::Cop::Style::EmptyLinesAroundBody,
     Rubocop::Cop::Style::EndOfLine,
     Rubocop::Cop::Style::FinalNewline,
     Rubocop::Cop::Style::HashSyntax,
+    Rubocop::Cop::Style::IfWithSemicolon,
     Rubocop::Cop::Style::IndentationWidth,
     Rubocop::Cop::Style::LineLength,
     Rubocop::Cop::Style::MethodCallParentheses,
     Rubocop::Cop::Style::MethodName,
     Rubocop::Cop::Style::Not,
+    Rubocop::Cop::Style::ParenthesesAroundCondition,
+    Rubocop::Cop::Style::ReduceArguments,
     Rubocop::Cop::Style::RedundantBegin,
-    Rubocop::Cop::Style::RedundantReturn,
     Rubocop::Cop::Style::RedundantSelf,
+    Rubocop::Cop::Style::RedundantReturn,
+    Rubocop::Cop::Style::SpaceAroundOperators,
+    Rubocop::Cop::Style::SpaceAroundBlockBraces,
+    Rubocop::Cop::Style::SpaceInsideParens,
+    Rubocop::Cop::Style::SpaceInsideBrackets,
+    Rubocop::Cop::Style::SpaceAfterColon,
+    Rubocop::Cop::Style::SpaceAfterComma,
     Rubocop::Cop::Style::SpaceAfterControlKeyword,
     Rubocop::Cop::Style::SpaceAfterMethodName,
     Rubocop::Cop::Style::SpaceAfterNot,
-    Rubocop::Cop::Style::SpaceAroundBlockBraces,
+    Rubocop::Cop::Style::SpaceAfterSemicolon,
     Rubocop::Cop::Style::SpaceAroundEqualsInParameterDefault,
-    Rubocop::Cop::Style::SpaceAroundOperators,
     Rubocop::Cop::Style::SpaceBeforeModifierKeyword,
-    Rubocop::Cop::Style::SpaceInsideBrackets,
     Rubocop::Cop::Style::SpaceInsideHashLiteralBraces,
-    Rubocop::Cop::Style::SpaceInsideParens,
     Rubocop::Cop::Style::StringLiterals,
+    Rubocop::Cop::Style::SymbolName,
     Rubocop::Cop::Style::Tab,
+    Rubocop::Cop::Style::TrailingBlankLines,
     Rubocop::Cop::Style::TrailingWhitespace,
-    Rubocop::Cop::Style::UnlessElse
+    Rubocop::Cop::Style::UnlessElse,
+    Rubocop::Cop::Style::VariableName
   ]
 
   def initialize(files)
@@ -70,8 +78,12 @@ class StyleChecker
   end
 
   def violations_in_file(source)
-    cops = RULES.map { |rule| rule.new }
-    commissioner = Rubocop::Cop::Commissioner.new(cops)
+    team = Rubocop::Cop::Team.new(RULES, configuration)
+    commissioner = Rubocop::Cop::Commissioner.new(team.cops)
     commissioner.investigate(source)
+  end
+
+  def configuration
+    @config ||= Rubocop::ConfigLoader.load_file('config/rubocop.yml')
   end
 end
