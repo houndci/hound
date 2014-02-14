5e91c3bd27817173ea69257cadabe30806faa51e
Scott Albertson <scott@thoughtbot.com>
2014-02-14 09:34:33 -0800

    Notify Sentry when background jobs error
    https://trello.com/c/Y97GquCc/105-make-sure-sentry-is-being-notified-of-exceptions-in-background-jobs

--- spec/jobs/build_job_spec.rb
+++ spec/jobs/build_job_spec.rb
@@ -1,6 +1,7 @@
-require 'spec_helper'
+require 'fast_spec_helper'
+require 'app/jobs/build_job'
 
-describe 'BuildJob', '#perform' do
+describe BuildJob, '#perform' do
   it 'runs the build' do
     build_runner = double(:build_runner, run: true)
     build_job = BuildJob.new(build_runner)
@@ -8,5 +9,17 @@ describe 'BuildJob', '#perform' do
     build_job.perform
 
     expect(build_runner).to have_received(:run)
+  end
+end
+
+describe BuildJob, '#error' do
+  it 'captures exception using the monitor' do
+    monitor = double(capture_exception: nil)
+    build_runner = double
+    sync_job = BuildJob.new(build_runner, monitor)
+
+    sync_job.error(double, StandardError)
+
+    expect(monitor).to have_received(:capture_exception).with(StandardError)
   end
 end
