4ce1824ecbccffcc37946f894b71bd1137473a23
Scott Albertson <scott@thoughtbot.com>
2014-02-21 15:44:04 -0800

    Change default mailer host

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -53,7 +53,7 @@ Houndapp::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'houndapp.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'staging.houndci.com' }
 
   # Enable threaded mode
   # config.threadsafe!
