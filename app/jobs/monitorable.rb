39a49985fa3fa0095d8c6d0764dfa6f6416e2de2
Scott Albertson <scott@thoughtbot.com>
2014-02-21 09:41:05 -0800

    Capture additional context with exception

--- app/jobs/monitorable.rb
+++ app/jobs/monitorable.rb
@@ -2,7 +2,7 @@ require 'sentry-raven'
 
 module Monitorable
   def error(job, exception)
-    monitor.capture_exception(exception)
+    monitor.capture_exception(exception, extra: { job_id: job.id })
   end
 
   private
