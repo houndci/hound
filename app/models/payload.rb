39609013e7c1340436951b21db2e05607a62ec0e
Scott Albertson <scott@thoughtbot.com>
2014-03-18 13:44:31 -0700

    Add action methods to Payload

--- app/models/payload.rb
+++ app/models/payload.rb
@@ -2,7 +2,11 @@ class Payload
   ALLOWED_ACTIONS = %w[opened synchronize]
 
   def initialize(payload_data)
-    @payload_data = payload_data
+    if payload_data.is_a? String
+      @payload_data = JSON.parse(payload_data)
+    else
+      @payload_data = payload_data
+    end
   end
 
   def head_sha
@@ -23,6 +27,14 @@ class Payload
 
   def valid_action?
     ALLOWED_ACTIONS.include?(action)
+  end
+
+  def opened?
+    action == 'opened'
+  end
+
+  def synchronize?
+    action == 'synchronize'
   end
 
   private
