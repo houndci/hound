117c9dfb9e90d92531e5f26e2d8fb756bea98ad6
Scott Albertson <scott@thoughtbot.com>
2014-03-27 07:44:26 -0700

    Test in RepoActivator changes

--- spec/services/repo_activator_spec.rb
+++ spec/services/repo_activator_spec.rb
@@ -4,35 +4,24 @@ describe RepoActivator do
   describe '#activate' do
     context 'when repo activation succeeds' do
       it 'activates repo' do
-        user = build(:user)
+        github_token = 'githubtoken'
         repo = create(:repo)
-        hook = double(id: 'hookid')
-        github = double(
-          :github_api,
-          create_pull_request_hook: hook
-        ).as_null_object
-        GithubApi.stub(:new).with(user.github_token).and_return(github)
+        stub_github_api
         activator = RepoActivator.new
 
-        activator.activate(repo, user)
+        activator.activate(repo, github_token)
 
+        expect(GithubApi).to have_received(:new).with(github_token)
         expect(repo.reload).to be_active
       end
 
       it 'creates GitHub hook' do
-        user = build(:user)
         repo = create(:repo)
-        hook = double(id: 'hookid')
-        github = double(
-          :github_api,
-          create_pull_request_hook: hook
-        ).as_null_object
-        GithubApi.stub(new: github)
+        github = stub_github_api
         activator = RepoActivator.new
 
-        activator.activate(repo, user)
+        activator.activate(repo, 'githubtoken')
 
-        expect(GithubApi).to have_received(:new).with(user.github_token)
         expect(github).to have_received(:create_pull_request_hook).with(
           repo.full_github_name,
           URI.join("http://#{ENV['HOST']}", 'builds').to_s
@@ -40,35 +29,21 @@ describe RepoActivator do
       end
 
       it 'makes Hound a collaborator' do
-        user = build(:user)
         repo = create(:repo)
-        hook = double(id: 'hookid')
-        github = double(
-          :github_api,
-          create_pull_request_hook: hook,
-          add_hound_to_repo: true
-        )
-        GithubApi.stub(new: github)
+        github = stub_github_api
         activator = RepoActivator.new
 
-        activator.activate(repo, user)
+        activator.activate(repo, 'githubtoken')
 
-        expect(GithubApi).to have_received(:new).with(user.github_token)
         expect(github).to have_received(:add_hound_to_repo)
       end
 
       it 'returns true if the repo activates successfully' do
-        user = build(:user)
         repo = create(:repo)
-        hook = double(id: 'hookid')
-        github = double(
-          :github_api,
-          create_pull_request_hook: hook
-        ).as_null_object
-        GithubApi.stub(new: github)
+        stub_github_api
         activator = RepoActivator.new
 
-        response = activator.activate(repo, user)
+        response = activator.activate(repo, 'githubtoken')
 
         expect(response).to be_true
       end
@@ -76,23 +51,23 @@ describe RepoActivator do
 
     context 'when repo activation fails' do
       it 'returns false if the repo does not activate successfully' do
-        user = double('user', github_token: nil)
+        github_token = nil
         repo = double('repo')
         expect(GithubApi).to receive(:new).and_raise(Octokit::Error.new)
         activator = RepoActivator.new
 
-        response = activator.activate(repo, user)
+        response = activator.activate(repo, github_token)
 
         expect(response).to be_false
       end
 
       it 'only swallows Octokit errors' do
-        user = double('user', github_token: nil)
+        github_token = 'githubtoken'
         repo = double('repo')
         expect(GithubApi).to receive(:new).and_raise(Exception.new)
         activator = RepoActivator.new
 
-        expect { activator.activate(repo, user) }.to raise_error(Exception)
+        expect { activator.activate(repo, github_token) }.to raise_error(Exception)
       end
     end
   end
@@ -101,12 +76,14 @@ describe RepoActivator do
     context 'when repo activation succeeds' do
       it 'deactivates repo' do
         stub_github_api
+        github_token = 'githubtoken'
         repo = create(:repo)
         create(:membership, repo: repo)
         activator = RepoActivator.new
 
-        activator.deactivate(repo)
+        activator.deactivate(repo, github_token)
 
+        expect(GithubApi).to have_received(:new).with(github_token)
         expect(repo.active?).to be_false
       end
 
@@ -116,7 +93,7 @@ describe RepoActivator do
         create(:membership, repo: repo)
         activator = RepoActivator.new
 
-        activator.deactivate(repo)
+        activator.deactivate(repo, 'githubtoken')
 
         expect(github_api).to have_received(:remove_pull_request_hook)
         expect(repo.hook_id).to be_nil
@@ -128,7 +105,7 @@ describe RepoActivator do
         create(:membership, repo: repo)
         activator = RepoActivator.new
 
-        response = activator.deactivate(repo)
+        response = activator.deactivate(repo, 'githubtoken')
 
         expect(response).to be_true
       end
@@ -136,32 +113,30 @@ describe RepoActivator do
 
     context 'when repo activation succeeds' do
       it 'returns false if the repo does not activate successfully' do
-        repo = double('repo', github_token: nil)
+        repo = double('repo')
+        github_token = nil
         expect(GithubApi).to receive(:new).and_raise(Octokit::Error.new)
         activator = RepoActivator.new
 
-        response = activator.deactivate(repo)
+        response = activator.deactivate(repo, github_token)
 
         expect(response).to be_false
       end
 
       it 'only swallows Octokit errors' do
-        repo = double('repo', github_token: nil)
+        repo = double('repo')
+        github_token = nil
         expect(GithubApi).to receive(:new).and_raise(Exception.new)
         activator = RepoActivator.new
 
-        expect { activator.deactivate(repo) }.to raise_error(Exception)
+        expect { activator.deactivate(repo, github_token) }.to raise_error(Exception)
       end
     end
   end
 
   def stub_github_api
     hook = double(:hook, id: 1)
-    api = double(
-      :github_api,
-      create_pull_request_hook: hook,
-      remove_pull_request_hook: nil
-    )
+    api = double(:github_api, create_pull_request_hook: hook).as_null_object
     GithubApi.stub(new: api)
     api
   end
