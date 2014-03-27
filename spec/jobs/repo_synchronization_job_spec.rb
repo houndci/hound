529bf2941bff8d920c166b5e0bbd5f7021bd06b6
Scott Albertson <scott@thoughtbot.com>
2014-03-27 07:55:53 -0700

    Test in repo sync changes

--- spec/jobs/repo_synchronization_job_spec.rb
+++ spec/jobs/repo_synchronization_job_spec.rb
@@ -11,13 +11,14 @@ end
 describe RepoSynchronizationJob, '#perform' do
   it 'syncs repos for a given user' do
     user = create(:user)
-    sync_job = RepoSynchronizationJob.new(user.id)
+    github_token = 'githubtoken'
+    sync_job = RepoSynchronizationJob.new(user.id, github_token)
     sync = double(start: nil)
     RepoSynchronization.stub(new: sync)
 
     sync_job.perform
 
-    expect(RepoSynchronization).to have_received(:new).with(user)
+    expect(RepoSynchronization).to have_received(:new).with(user, github_token)
     expect(sync).to have_received(:start)
   end
 end
--- spec/services/repo_synchronization_spec.rb
+++ spec/services/repo_synchronization_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe RepoSynchronization do
   describe '#start' do
     it 'replaces existing repos' do
-      user = create(:user, github_token: 'token')
+      user = create(:user)
+      github_token = 'githubtoken'
       existing_repo = create(:repo, github_id: 123)
       user.repos << existing_repo
       api = double(
@@ -13,11 +14,11 @@ describe RepoSynchronization do
         ]
       )
       GithubApi.stub(new: api)
-      synchronization = RepoSynchronization.new(user)
+      synchronization = RepoSynchronization.new(user, github_token)
 
       synchronization.start
 
-      expect(GithubApi).to have_received(:new).with(user.github_token)
+      expect(GithubApi).to have_received(:new).with(github_token)
       expect(user).to have(1).repo
       expect(user.repos.first.full_github_name).to eq 'user/newrepo'
     end
@@ -25,15 +26,16 @@ describe RepoSynchronization do
     describe 'when a repo membership already exists' do
       it 'creates another membership' do
         repo = create(:repo)
-        first_user = create(:user, github_token: 'token')
-        second_user = create(:user, github_token: 'token')
+        first_user = create(:user)
+        second_user = create(:user)
+        github_token = 'githubtoken'
         first_user.repos << repo
         api = double(
           :github_api,
           repos: [{id: repo.github_id, name: repo.name, full_name: repo.full_github_name}]
         )
         GithubApi.stub(new: api)
-        synchronization = RepoSynchronization.new(second_user)
+        synchronization = RepoSynchronization.new(second_user, github_token)
 
         synchronization.start
 
