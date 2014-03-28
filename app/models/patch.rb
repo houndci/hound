2d4f77fec8ea26ecdb6c40050a5bf234780d9743
Scott Albertson <scott@thoughtbot.com>
2014-03-28 08:20:10 -0700

    Don't parse in initialize

--- app/models/patch.rb
+++ app/models/patch.rb
@@ -5,31 +5,29 @@ class Patch
 
   Line = Struct.new(:content, :line_number, :patch_position)
 
-  attr_reader :additions
-
   def initialize(body)
     @body = body
-    @additions = []
-    parse
   end
 
-  private
-
-  def parse
+  def additions
     line_number = 0
 
-    lines.each_with_index do |content, patch_position|
+    lines.each_with_index.inject([]) do |additions, (content, patch_position)|
       case content
       when RANGE_INFORMATION_LINE
         line_number = Regexp.last_match[:line_number].to_i
       when MODIFIED_LINE
-        @additions << Line.new(content, line_number, patch_position)
+        additions << Line.new(content, line_number, patch_position)
         line_number += 1
       when NOT_REMOVED_LINE
         line_number += 1
       end
+
+      additions
     end
   end
+
+  private
 
   def lines
     @body.lines
