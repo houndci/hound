aa4de8f6159273fad0dfe3629a8b0e1722cf10de
Laila Winner <laila@thoughtbot.com>
2014-03-28 16:31:07 -0700

    Initiate sync process on client side

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,4 @@
 class HomeController < ApplicationController
   def index
-    if current_user.repos.empty?
-      enqueue_repo_sync_job
-    end
-  end
-
-  private
-
-  def enqueue_repo_sync_job
-    sync_job = RepoSynchronizationJob.new(current_user.id)
-    Delayed::Job.enqueue(sync_job)
   end
 end
