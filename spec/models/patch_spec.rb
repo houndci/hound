7781726a89e9502b0d306596e1b520f85f90a957
Scott Albertson <scott@thoughtbot.com>
2014-04-07 18:35:24 -0700

    Handle nil body in Patch

--- spec/models/patch_spec.rb
+++ spec/models/patch_spec.rb
@@ -1,5 +1,6 @@
 require 'fast_spec_helper'
 require 'app/models/patch'
+require 'app/models/line'
 
 describe Patch do
   describe '#additions' do
@@ -10,6 +11,14 @@ describe Patch do
       expect(patch).to have(3).additions
       expect(patch.additions.map(&:line_number)).to eq [14, 22, 54]
       expect(patch.additions.map(&:patch_position)).to eq [5, 13, 37]
+    end
+
+    context 'when body is nil' do
+      it 'returns no additions' do
+        patch = Patch.new(nil)
+
+        expect(patch).to have(0).additions
+      end
     end
   end
 end
