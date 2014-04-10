68e2aefe3db5a92956c19f9ab80f0bc910678762
Greg Lazarev <greg@thoughtbot.com>
2014-04-10 12:11:36 -0700

    Show active repos first in the list

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -2,6 +2,6 @@ class ReposController < ApplicationController
   respond_to :json
 
   def index
-    respond_with current_user.repos.order(:full_github_name)
+    respond_with current_user.repos.order(active: :desc, full_github_name: :asc)
   end
 end
