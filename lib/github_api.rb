3cd34ed464681dd4df046458342b8826f1b44c91
Laila Winner <laila@thoughtbot.com>
2014-04-12 12:30:52 -0700

    Style improvements

--- lib/github_api.rb
+++ lib/github_api.rb
@@ -81,9 +81,9 @@ class GithubApi
 
   def admin_team_ids(repo_teams)
     teams = client.user_teams
-    admin_teams = teams.keep_if { |team| team.permission == 'admin' }
-    admin_team_ids = admin_teams.map { |team| team.id }
-    repo_team_ids = repo_teams.map { |team| team.id }
+    admin_teams = teams.select { |team| team.permission == 'admin' }
+    admin_team_ids = admin_teams.map(&:id)
+    repo_team_ids = repo_teams.map(&:id)
     admin_team_ids & repo_team_ids
   end
 
