15476f2d9d9e45e4875c0d2f06be67583a68fb41
Laila Winner <laila@thoughtbot.com>
2014-04-11 19:54:13 -0700

    Fix errors

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -1,12 +1,10 @@
-require 'github_api'
-
 class GithubUser
   def initialize(client)
     @client = client
   end
 
   def admin_access_teams
-    teams = client.user_teams
+    teams = @client.user_teams
     teams.keep_if { |team| team.permission == 'admin' }
   end
 end
--- spec/models/github_user_spec.rb
+++ spec/models/github_user_spec.rb
@@ -1,5 +1,4 @@
 require 'fast_spec_helper'
-require 'lib/github_user'
 require 'json'
 
 describe GithubUser, '#admin_access_teams' do
