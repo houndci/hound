f465a62b13681de48f3ae7b3cf5ea0bb316b3e29
Scott Albertson <scott@thoughtbot.com>
2014-04-17 11:31:03 -0700

    Cache responses even when Cache-Control is private
    * Cache requests to limit requests that count against rate limit
    * Use Marshal for serialization instead of JSON because of serialization issue
    
    https://github.com/plataformatec/faraday-http-cache/issues/38

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -1,5 +1,10 @@
 stack = Faraday::Builder.new do |builder|
-  builder.use Faraday::HttpCache, store: Rails.cache
+  builder.use(
+    Faraday::HttpCache,
+    store: Rails.cache,
+    shared_cache: false,
+    serializer: Marshal
+  )
   builder.use Octokit::Response::RaiseError
   builder.adapter Faraday.default_adapter
 end
