9500c555a836372201d156ae21a5a06f98363748
Simon Taranto <simon@thoughtbot.com>
2014-04-17 13:41:54 -0700

    rename repo if name changed on github

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,6 +1,4 @@
 class Repo < ActiveRecord::Base
-  include ActiveModel::ForbiddenAttributesProtection
-
   has_many :memberships
   has_many :users, through: :memberships
   has_many :builds
@@ -12,5 +10,13 @@ class Repo < ActiveRecord::Base
 
   def deactivate
     update_attributes(active: false, hook_id: nil)
+  end
+
+  def update_changed_attributes(new_attributes)
+    new_full_github_name = new_attributes[:full_name]
+
+    if full_github_name != new_full_github_name
+      update_attributes(full_github_name: new_full_github_name)
+    end
   end
 end
--- spec/services/repo_synchronization_spec.rb
+++ spec/services/repo_synchronization_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe RepoSynchronization do
   describe '#start' do
     it 'replaces existing repos' do
-      user = create(:user)
       github_token = 'githubtoken'
-      existing_repo = create(:repo, github_id: 123)
-      user.repos << existing_repo
+      membership = create(:membership)
+      user = membership.user
+
       api = double(
         :github_api,
         repos: [
@@ -21,19 +21,44 @@ describe RepoSynchronization do
       expect(GithubApi).to have_received(:new).with(github_token)
       expect(user).to have(1).repo
       expect(user.repos.first.full_github_name).to eq 'user/newrepo'
+      expect(user.repos.first.github_id).to eq 456
+    end
+
+    it 'renames an existing repo if updated on github' do
+      github_token = 'githubtoken'
+      membership = create(:membership)
+
+      api = double(
+        :github_api,
+        repos: [
+          {
+            name: 'New Name',
+            full_name: 'user/newname',
+            id: membership.repo.github_id
+          }
+        ]
+      )
+      GithubApi.stub(new: api)
+      synchronization = RepoSynchronization.new(membership.user, github_token)
+
+      synchronization.start
+
+      expect(membership.user.repos.first.full_github_name).to eq 'user/newname'
+      expect(membership.user.repos.first.github_id).
+        to eq membership.repo.github_id
     end
 
     describe 'when a repo membership already exists' do
       it 'creates another membership' do
-        repo = create(:repo)
-        first_user = create(:user)
-        second_user = create(:user)
         github_token = 'githubtoken'
-        first_user.repos << repo
+        first_membership = create(:membership)
+        repo = first_membership.repo
+        second_user = create(:user)
         api = double(
           :github_api,
           repos: [{ id: repo.github_id, full_name: repo.full_github_name }]
         )
+
         GithubApi.stub(new: api)
         synchronization = RepoSynchronization.new(second_user, github_token)
 
