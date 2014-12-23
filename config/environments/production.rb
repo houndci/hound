830af2bf3bef41687fde45f8c6783fe1ec706d7c
Arun Agrawal <arunagw@gmail.com>
2014-12-23 13:08:20 +0100

    Bump rails version to 4.1.8
    * Using secrets.yml which added in 4.1
    * Using assets.rb inside initializers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,8 +47,6 @@ Houndapp::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( repos/*.js )
   config.assets.js_compressor = :uglifier
 
   # Disable delivery errors, bad email addresses will be ignored
---
+++ config/initializers/assets.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = "1.0"
+
+# Precompile additional assets.
+Rails.application.config.assets.precompile += %w( repos/*.js )
