bd843c3c1e2dc2fc67c1dc66d2d74bbaa6c75463
Arun Agrawal <arunagw@gmail.com>
2014-12-23 16:00:50 +0100

    Bump Rails version to 4.2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,7 +9,7 @@ Houndapp::Application.configure do
   config.cache_classes = true
 
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.static_cache_control = "public, max-age=3600"
 
   # Show full error reports and disable caching
