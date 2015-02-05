6303c8fce8e4049189e455d370462081ca90668c
Scott Albertson <hello@scottalbertson.com>
2015-02-04 22:25:07 -0800

    Update unsupported style guide test

--- app/models/style_guide/unsupported.rb
+++ app/models/style_guide/unsupported.rb
@@ -1,6 +1,10 @@
 # Returns empty set of violations.
 module StyleGuide
   class Unsupported < Base
+    CONFIG_FILE = ""
+
+    pattr_initialize :config
+
     def violations_in_file(_)
       []
     end
