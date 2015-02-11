209868e115c992691cc574f00bd533cb56d0579e
Arun Agrawal <arunagw@gmail.com>
2015-02-11 11:01:41 +0100

    Backfill repo and memberships timestamp from users

---
+++ db/migrate/20150211092151_backfill_timestamp_for_repos.rb
@@ -0,0 +1,55 @@
+class BackfillTimestampForRepos < ActiveRecord::Migration
+  def up
+    sql = <<-SQL
+      UPDATE memberships
+        SET created_at = users.created_at
+        FROM users
+        WHERE memberships.user_id = users.id
+        AND memberships.created_at is NULL;
+
+      UPDATE memberships
+        SET updated_at = users.updated_at
+        FROM users
+        WHERE memberships.user_id = users.id
+        AND memberships.updated_at is NULL;
+
+      UPDATE repos
+        SET created_at = memberships.created_at
+        FROM memberships
+        WHERE repos.id = memberships.repo_id
+        AND repos.created_at is NULL;
+
+      UPDATE repos
+        SET updated_at = memberships.updated_at
+        FROM memberships
+        WHERE repos.id = memberships.repo_id
+        AND repos.updated_at is NULL;
+    SQL
+
+    ActiveRecord::Base.connection.execute(sql)
+
+    remaining_updates = <<-SQL
+      UPDATE repos
+        SET created_at = (select current_timestamp)
+        WHERE created_at is NULL;
+
+      UPDATE repos
+        SET updated_at = (select current_timestamp)
+        WHERE updated_at is NULL;
+
+      UPDATE memberships
+        SET created_at = (select current_timestamp)
+        WHERE created_at is NULL;
+
+      UPDATE memberships
+        SET updated_at = (select current_timestamp)
+        WHERE updated_at is NULL;
+    SQL
+
+    ActiveRecord::Base.connection.execute(remaining_updates)
+  end
+
+  def down
+    ActiveRecord::IrreversibleMigration
+  end
+end
