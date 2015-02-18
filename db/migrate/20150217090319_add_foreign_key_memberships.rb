1278a0f0421b0ee3e070347a4aa5eb48ab61c2dd
Arun Agrawal <arunagw@gmail.com>
2015-02-18 13:26:26 +0100

    Use rake task for cleanup

--- db/migrate/20150217090319_add_foreign_key_memberships.rb
+++ db/migrate/20150217090319_add_foreign_key_memberships.rb
@@ -1,16 +1,6 @@
 class AddForeignKeyMemberships < ActiveRecord::Migration
-  def up
-    ActiveRecord::Base.connection.execute <<-SQL
-      DELETE FROM memberships
-      WHERE (repo_id NOT IN (SELECT id FROM repos))
-    SQL
-
+  def change
     add_foreign_key :memberships, :users
     add_foreign_key :memberships, :repos
-  end
-
-  def down
-    remove_foreign_key :memberships, :users
-    remove_foreign_key :memberships, :repos
   end
 end
