31f8f133d8d6a26b949ae803771eb542ad6146de
Greg Lazarev <greg@thoughtbot.com>
2015-02-20 16:15:59 -0800

    Reduce the wait time between failed jobs
    Most of the failures we get are network problems. We shouldn't wait long to
    retry jobs anyhow.

--- app/jobs/retryable.rb
+++ app/jobs/retryable.rb
@@ -4,6 +4,6 @@ module Retryable
   extend Resque::Plugins::Retry
 
   @retry_limit = 10
-  @retry_delay = 120
+  @retry_delay = ENV.fetch("RESQUE_RETRY_DELAY", 30).to_i
   @fatal_exceptions = [Octokit::Unauthorized]
 end
