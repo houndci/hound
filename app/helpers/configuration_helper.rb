d86435ba7be7872854d4d7756d8b075e5b6cda59
Scott Albertson <ascottalbertson@gmail.com>
2015-02-23 21:28:13 -0800

    Use complete configuration files
    * Use complete configuration files for Hound defaults.
    * Update documentation to include examples of complete configs.

---
+++ app/helpers/configuration_helper.rb
@@ -0,0 +1,21 @@
+module ConfigurationHelper
+  def ruby_config_url
+    "https://raw.githubusercontent.com/thoughtbot/hound/master/config/style_guides/ruby.yml"
+  end
+
+  def coffeescript_config_url
+    "https://raw.githubusercontent.com/thoughtbot/hound/master/config/style_guides/coffeescript.json"
+  end
+
+  def javascript_config_url
+    "https://raw.githubusercontent.com/thoughtbot/hound/master/config/style_guides/javascript.json"
+  end
+
+  def javascript_ignore_url
+    "https://raw.githubusercontent.com/thoughtbot/hound/master/config/style_guides/.jshintignore"
+  end
+
+  def scss_config_url
+    "https://raw.githubusercontent.com/thoughtbot/hound/master/config/style_guides/scss.yml"
+  end
+end
