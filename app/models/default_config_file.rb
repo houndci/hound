bd90f14dd3d729aa5ca1d3dd418db2840064d847
David Backeus <duztdruid@gmail.com>
2015-02-25 13:20:38 +0100

    Have mnd-hound always use mynewsdesk styles regardless of owner

--- app/models/default_config_file.rb
+++ app/models/default_config_file.rb
@@ -1,6 +1,5 @@
 class DefaultConfigFile
-  CONFIG_DIR = "config/style_guides"
-  MYNEWSDESK_CONFIG_DIR = "config/style_guides/mynewsdesk"
+  CONFIG_DIR = "config/style_guides/mynewsdesk"
   THOUGHTBOT_CONFIG_DIR = "config/style_guides/thoughtbot"
 
   pattr_initialize :file_name, :repository_owner_name
@@ -14,15 +13,9 @@ class DefaultConfigFile
   def directory
     if thoughtbot_repository?
       THOUGHTBOT_CONFIG_DIR
-    elsif mynewsdesk_repository?
-      MYNEWSDESK_CONFIG_DIR
     else
       CONFIG_DIR
     end
-  end
-
-  def mynewsdesk_repository?
-    repository_owner_name == "mynewsdesk"
   end
 
   def thoughtbot_repository?
