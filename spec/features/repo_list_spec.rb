48340caa77092acb7b2d99daca2766e5b0885d8e
Galen Frechette <galenfrechette@gmail.com>
2015-03-06 10:57:22 -0800

    Add basic onboarding to dashboard
    * Add logic for displaying onboarding and specific messaging
    * Add a pulse-in animation for the onboarding UI
    * Add specs for checking visibility of onboarding

--- spec/features/repo_list_spec.rb
+++ spec/features/repo_list_spec.rb
@@ -14,6 +14,26 @@ feature "Repo list", js: true do
     expect(page).to have_content repo.full_github_name
   end
 
+  scenario "user sees onboarding" do
+    user = create(:user)
+    sign_in_as(user)
+
+    visit repos_path
+
+    expect(page).to have_content I18n.t("onboarding.title")
+  end
+
+  scenario "user does not see onboarding" do
+    user = create(:user)
+    build = create(:build)
+    build.repo.users << user
+    sign_in_as(user)
+
+    visit repos_path
+
+    expect(page).to_not have_content I18n.t("onboarding.title")
+  end
+
   scenario "user views list" do
     user = create(:user)
     repo = create(:repo, full_github_name: "thoughtbot/my-repo")
