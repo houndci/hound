a312523589ef846827c8bc652bb57661a5be2318
Teo Ljungberg <teo@teoljungberg.com>
2015-03-27 13:40:18 +0100

    Proper contexts for BuildWorkersControllerSpec

--- spec/controllers/build_workers_controller_spec.rb
+++ spec/controllers/build_workers_controller_spec.rb
@@ -2,51 +2,53 @@ require "rails_helper"
 
 describe BuildWorkersController do
   describe "#update" do
-    context "auth" do
-      it "unauthorized access without token" do
+    context "when not authorized" do
+      it "responds with 401" do
         put :update, id: 1, format: :json
 
         expect(response.status).to eq(401)
       end
     end
 
-    context "given an completed build" do
-      it "does not complete the build" do
-        build_worker = create(
-          :build_worker,
-          :completed,
-          completed_at: 1.day.ago,
-        )
+    context "when authorized" do
+      context "given a completed build" do
+        it "does not complete the build" do
+          build_worker = create(
+            :build_worker,
+            :completed,
+            completed_at: 1.day.ago,
+          )
+          authorized_headers_for_build_worker
+
+          put :update, id: build_worker.id, format: :json
+
+          expect(response.status).to eq 409
+          expect(build_worker.completed_at).to be < Time.now
+          expect(json_body["error"]).to eq(
+            "BuildWorker##{build_worker.id} has already been finished"
+          )
+        end
+      end
+
+      it "dispatches a ReviewJob" do
+        allow(ReviewJob).to receive(:perform_later)
+        file = double("File")
+        violations = double("ViolationsAttrs")
+        build_worker = create(:build_worker)
         authorized_headers_for_build_worker
 
-        put :update, id: build_worker.id, format: :json
-
-        expect(response.status).to eq 409
-        expect(build_worker.completed_at).to be < Time.now
-        expect(json_body["error"]).to eq(
-          "BuildWorker##{build_worker.id} has already been finished"
+        put(
+          :update,
+          id: build_worker.id,
+          violations: violations,
+          file: file,
+          format: :json,
         )
+
+        expect(ReviewJob).
+          to have_received(:perform_later).
+          with(build_worker, file, violations)
       end
-    end
-
-    it "dispatches a ReviewJob" do
-      allow(ReviewJob).to receive(:perform_later)
-      file = double("File")
-      violations = double("ViolationsAttrs")
-      build_worker = create(:build_worker)
-      authorized_headers_for_build_worker
-
-      put(
-        :update,
-        id: build_worker.id,
-        violations: violations,
-        file: file,
-        format: :json,
-      )
-
-      expect(ReviewJob).
-        to have_received(:perform_later).
-        with(build_worker, file, violations)
     end
   end
 end
