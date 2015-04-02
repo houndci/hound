5b1c87b66bf0f25f55e02a9904d59c4f37d4450e
Greg Lazarev <greg@thoughtbot.com>
2015-04-02 14:43:44 -0700

    Use absolute file paths to fix excluding
    We call `make_excludes_absolute` on the configuration
    to satisfy RuboCop's `file_to_exclude?` since it calls `File.expand_path`
    on the filename that we pass it.
    Thus, all of our `Exclude` paths for individual cops get expanded to absolute as
    well. We need to call `File.expand_path` on the filename in order for the two
    paths to match.
    
    This is kind of silly, since GitHub does not give us absolute paths, so we
    should be fine with just the relative paths (relative to project root),
    because they would match what the user puts in their config.
    
    Basically, all of this (plus `make_excludes_absolute`) is because of
    `file_to_exclude?`.

--- app/models/style_guide/ruby.rb
+++ app/models/style_guide/ruby.rb
@@ -28,7 +28,8 @@ module StyleGuide
     end
 
     def parsed_source(file)
-      RuboCop::ProcessedSource.new(file.content, file.filename)
+      absolute_filepath = File.expand_path(file.filename)
+      RuboCop::ProcessedSource.new(file.content, absolute_filepath)
     end
 
     def config
