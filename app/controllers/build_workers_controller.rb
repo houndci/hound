006e7e181a792760700c507cd2e29d19f59d5704
Chris Thorn <chris@thorn.co>
2015-04-02 12:08:57 -0700

    Use secure comparison for worker tokens

--- app/controllers/build_workers_controller.rb
+++ app/controllers/build_workers_controller.rb
@@ -34,7 +34,12 @@ class BuildWorkersController < ApplicationController
 
   def authenticate_with_token
     authenticate_or_request_with_http_token do |token, _|
-      token == ENV.fetch("BUILD_WORKERS_TOKEN")
+      token.size == build_workers_token.size &&
+        ActiveSupport::SecurityUtils.secure_compare(token, build_workers_token)
     end
+  end
+
+  def build_workers_token
+    ENV.fetch("BUILD_WORKERS_TOKEN")
   end
 end
