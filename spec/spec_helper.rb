081fcb642ccd291e58027e5d4b6d19d8522ebf2e
Greg Lazarev <greg@thoughtbot.com>
2015-04-03 15:19:11 -0700

    Move test ENV config vars to `.env.test`
    Jasmine Rails runner requires `SECRET_KEY_BASE` to be set
    when it runs on Travis.
    Moving things into `.env.test` ensures the keys are loaded in test env,
    and not dependent on `spec_helper`.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 $: << File.expand_path("../..", __FILE__)
 
+ENV["EXEMPT_ORGS"] = "thoughtbot,billybob"
+
 require "attr_extras"
 require "byebug"
 require "webmock/rspec"
@@ -7,17 +9,6 @@ require "active_support"
 require "active_support/core_ext"
 
 Dir["spec/support/**/*.rb"].each { |f| require f }
-
-ENV["HOST"] = "test.host"
-ENV["SECRET_KEY_BASE"] = "test-key" * 5
-ENV["HOUND_GITHUB_USERNAME"] = "houndci"
-ENV["HOUND_GITHUB_TOKEN"] = "houndgithubtoken"
-ENV["ENABLE_HTTPS"] = "no"
-ENV["CHANGED_FILES_THRESHOLD"] = "300"
-ENV["MAX_COMMENTS"] = "10"
-ENV["STRIPE_API_KEY"] = "sk_test_123"
-ENV["STRIPE_PUBLISHABLE_KEY"] = "pk_test_123"
-ENV["EXEMPT_ORGS"] = "thoughtbot,billybob"
 
 RSpec.configure do |config|
   config.order = "random"
