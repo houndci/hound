69a7946941c8086c4e79c4bc1228616d9fe6d384
Jens Debergh <jens@typework.com>
2015-05-01 20:07:01 +0200

    Set deploy master branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :stage, :production
 
 server "188.226.154.160", user: "typework", roles: %w{web app db}
-set :branch, "jd-add-deployment-scripts"
+set :branch, "master"
 
 set :nginx_server_name, "188.226.154.160"
 set :nginx_use_spdy, false
