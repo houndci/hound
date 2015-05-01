a29f8b9b118795f47bca5e4e878129b5bb2773dd
Jens Debergh <jens@typework.com>
2015-05-01 16:44:21 +0200

    Missing dotenv

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ set :stage, :production
 server "188.226.154.160", user: "typework", roles: %w{web app db}
 set :branch, "jd-add-deployment-scripts"
 
-set :nginx_server_name, "hound"
+set :nginx_server_name, "188.226.154.160"
 set :nginx_use_spdy, false
 set :nginx_enable_pagespeed, false
 
