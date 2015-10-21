houndci/hound

commit fec130bfe8f411c22681cfe74f7cfd133c3b5ca4

Introduce a `Config` class for each `Linter`
To decouple the different kinds of configuration options Hound supports,
let each linter decide on the specific kind of configuration it needs.
Both in what format, how to parse it, and how to access it.

Changes:

- Add `HoundConfig` to be the object representation of `.hound.yml`.
- Add `Config::LINTER` to represent the configuration for the specific `LINTER`.
- Each subclass of `Config::Base` implements their own `parse` method,
  that takes the content of the file it should parse, and do so
  accordingly.
- Wrap parsers in `Config::Parser`.
- Replace, and remove, `RepoConfig` with `HoundConfig` and/or
  `Config::LINTER` (where applicable).
- Remove `NAME` constant from `Linter::*`. `NAME` was used to set
  `Linter::Base#_linter_name`, which can be determined by
  `Linter::Base#name` instead.

https://trello.com/c/PoRwwKIw

app/models/config/ruby.rb
@@ -1,0 +1,27 @@
+module Config
+  class Ruby < Base
+    def content
+      if legacy?
+        hound_config.content
+      else
+        super
+      end
+    end
+
+    private
+
+    def parse(file_content)
+      result = Parser.yaml(file_content)
+
+      ensure_correct_type(result)
+    end
+
+    def legacy?
+      (configured_languages & HoundConfig::LANGUAGES).empty?
+    end
+
+    def configured_languages
+      hound_config.content.keys
+    end
+  end
+end

spec/models/config/ruby_spec.rb
@@ -1,0 +1,81 @@
+require "spec_helper"
+require "app/models/config/base"
+require "app/models/config/ruby"
+require "app/models/hound_config"
+require "app/models/config/parser_error"
+
+describe Config::Ruby do
+  describe "#content" do
+    context "when the hound config is a legacy config" do
+      it "returns the HoundConfig's content as a hash" do
+        hound_config = double(
+          "HoundConfig",
+          content: { "LineLength" => { "Max" => 90 } },
+        )
+        config = Config::Ruby.new(hound_config, "ruby")
+
+        expect(config.content).to eq("LineLength" => { "Max" => 90 })
+      end
+    end
+
+    context "when the hound config is not a legacy config" do
+      it "returns the content from GitHub as a hash" do
+        commit = stubbed_commit(
+          "config/rubocop.yml" => '{ "LineLength": { "Max": 90 } }',
+        )
+        config = build_config(commit)
+
+        expect(config.content).to eq("LineLength" => { "Max" => 90 })
+      end
+    end
+  end
+
+  context "when the given content is invalid" do
+    context "when the result is not a hash" do
+      it "raises a type exception" do
+        commit = stubbed_commit(
+          "config/rubocop.yml" => <<-EOS.strip_heredoc
+            !
+          EOS
+        )
+        config = build_config(commit)
+
+        expect { config.content }.to raise_error(
+          Config::ParserError,
+          %r(`config/rubocop.yml` must be a Hash),
+        )
+      end
+    end
+
+    context "when the content is invalid yaml" do
+      it "raises an exception" do
+        commit = stubbed_commit(
+          "config/rubocop.yml" => <<-EOS.strip_heredoc
+            ruby: !ruby/object
+              ;foo:
+          EOS
+        )
+        config = build_config(commit)
+
+        expect { config.content }.to raise_error(
+          Config::ParserError,
+          /Tried to load unspecified class: Object/,
+        )
+      end
+    end
+  end
+
+  def build_config(commit)
+    hound_config = double(
+      "HoundConfig",
+      commit: commit,
+      content: {
+        "ruby" => {
+          "enabled" => true,
+          "config_file" => "config/rubocop.yml",
+        },
+      },
+    )
+    Config::Ruby.new(hound_config, "ruby")
+  end
+end

app/models/linter/ruby.rb
@@ -9,7 +9,7 @@ module Linter
     end
 
     def file_included?(commit_file)
-      !config.file_to_exclude?(commit_file.filename)
+      !linter_config.file_to_exclude?(commit_file.filename)
     end
 
     private
@@ -27,7 +27,11 @@ module Linter
     end
 
     def team
-      RuboCop::Cop::Team.new(RuboCop::Cop::Cop.all, config, rubocop_options)
+      RuboCop::Cop::Team.new(
+        RuboCop::Cop::Cop.all,
+        linter_config,
+        rubocop_options,
+      )
     end
 
     def parsed_source(commit_file)
@@ -35,8 +39,8 @@ module Linter
       RuboCop::ProcessedSource.new(commit_file.content, absolute_filepath)
     end
 
-    def config
-      @config ||= RuboCop::Config.new(merged_config, "")
+    def linter_config
+      @linter_config ||= RuboCop::Config.new(merged_config, "")
     end
 
     def merged_config
@@ -50,9 +54,9 @@ module Linter
     end
 
     def custom_config
-      RuboCop::Config.new(repo_config.for(name), "").tap do |config|
-        config.add_missing_namespaces
-        config.make_excludes_absolute
+      RuboCop::Config.new(config.content, "").tap do |custom_config|
+        custom_config.add_missing_namespaces
+        custom_config.make_excludes_absolute
       end
     rescue NoMethodError
       RuboCop::Config.new
@@ -61,7 +65,7 @@ module Linter
     # This is deprecated in favor of RuboCop's DisplayCopNames option.
     # Let's track how often we see this and remove it if we see fit.
     def rubocop_options
-      if config.delete("ShowCopNames")
+      if linter_config.delete("ShowCopNames")
         Analytics.new(repository_owner_name).track_show_cop_names
         { debug: true }
       end

spec/models/linter/ruby_spec.rb
@@ -69,9 +69,9 @@ describe Linter::Ruby do
       describe "for single line conditional" do
         it "returns no violations" do
           expect(violations_in(<<-CODE.strip_heredoc)).to eq []
-  if signed_in? then redirect_to dashboard_path end
+            if signed_in? then redirect_to dashboard_path end
 
-  while signed_in? do something end
+            while signed_in? do something end
           CODE
         end
       end
@@ -79,9 +79,9 @@ describe Linter::Ruby do
       describe "for has_* method name" do
         it "returns no violations" do
           expect(violations_in(<<-CODE.strip_heredoc)).to eq []
-  def has_something?
-    "something"
-  end
+            def has_something?
+              "something"
+            end
           CODE
         end
       end
@@ -459,7 +459,10 @@ describe Linter::Ruby do
               end
             CODE
 
-            violations = violations_in(code, repository_owner_name: "thoughtbot")
+            violations = violations_in(
+              code,
+              repository_owner_name: "thoughtbot",
+            )
 
             expect(violations).to eq [
               "Use 2 (not 7) spaces for indenting an expression " +
@@ -472,19 +475,21 @@ describe Linter::Ruby do
 
     context "with custom configuration" do
       it "finds only one violation" do
-        config = {
+        config = stub_ruby_config(
           "StringLiterals" => {
             "EnforcedStyle" => "double_quotes"
           }
-        }
+        )
 
-        violations = violations_with_config(config)
+        violations = violations_with_config(config: config)
 
         expect(violations).to eq ["Use the new Ruby 1.9 hash syntax."]
       end
 
       it "can use custom configuration to display rubocop cop names" do
-        config = { "AllCops" => { "DisplayCopNames" => "true" } }
+        config = stub_ruby_config(
+          "AllCops" => { "DisplayCopNames" => "true" },
+        )
 
         violations = violations_with_config(config)
 
@@ -495,14 +500,14 @@ describe Linter::Ruby do
 
       context "with old-style syntax" do
         it "has one violation" do
-          config = {
+          config = stub_ruby_config(
             "StringLiterals" => {
-              "EnforcedStyle" => "single_quotes"
+              "EnforcedStyle" => "single_quotes",
             },
             "HashSyntax" => {
-              "EnforcedStyle" => "hash_rockets"
+              "EnforcedStyle" => "hash_rockets",
             },
-          }
+          )
 
           violations = violations_with_config(config)
 
@@ -561,16 +566,6 @@ describe Linter::Ruby do
           CODE
         end
       end
-
-      def violations_with_config(config)
-        content = <<-TEXT.strip_heredoc
-          def test_method
-            { :foo => "hello world" }
-          end
-        TEXT
-
-        violations_in(content, config: config)
-      end
     end
 
     context "default configuration" do
@@ -599,7 +594,6 @@ describe Linter::Ruby do
       it "uses the thoughtbot configuration for rubocop" do
         spy_on_rubocop_team
         spy_on_rubocop_configuration_loader
-        config_file = thoughtbot_configuration_file(Linter::Ruby)
         code = <<-CODE.strip_heredoc
           private def foo
             bar
@@ -609,11 +603,15 @@ describe Linter::Ruby do
         thoughtbot_violations_in(code)
 
         expect(RuboCop::ConfigLoader).to(
-          have_received(:configuration_from_file).with(config_file)
+          have_received(:configuration_from_file).with(
+            thoughtbot_configuration_file(Linter::Ruby),
+          ).at_least(:once),
         )
-
-        expect(RuboCop::Cop::Team).to have_received(:new).
-          with(anything, thoughtbot_configuration, anything)
+        expect(RuboCop::Cop::Team).to have_received(:new).with(
+          anything,
+          thoughtbot_configuration,
+          anything,
+        )
       end
 
       describe "when using reduce" do
@@ -628,13 +626,13 @@ describe Linter::Ruby do
 
       describe "when using inject" do
         it "returns violations" do
-          violations = ["Prefer `reduce` over `inject`."]
           code = <<-CODE.strip_heredoc
             users.inject(0) do |_, user|
               user.age
             end
           CODE
 
+          violations = ["Prefer `reduce` over `inject`."]
           expect(thoughtbot_violations_in(code)).to eq violations
         end
       end
@@ -665,21 +663,22 @@ describe Linter::Ruby do
       end
 
       def thoughtbot_violations_in(content)
-        violations_in(content, repository_owner_name: "thoughtbot")
+        violations_in(
+          content,
+          repository_owner_name: "thoughtbot",
+          config: stub_ruby_config(thoughtbot_configuration),
+        )
       end
     end
 
     describe "#file_included?" do
       context "with excluded file" do
         it "returns false" do
-          config = {
-            "AllCops" => {
-              "Exclude" => ["ignore.rb"]
-            }
-          }
-          repo_config = double("RepoConfig", for: config)
+          config = stub_ruby_config(
+            "AllCops" => { "Exclude" => ["ignore.rb"] },
+          )
           file = double("CommitFile", filename: "ignore.rb")
-          linter = build_linter(repo_config: repo_config)
+          linter = build_linter(config: config)
 
           expect(linter.file_included?(file)).to eq false
         end
@@ -687,14 +686,9 @@ describe Linter::Ruby do
 
       context "with included file" do
         it "returns true" do
-          config = {
-            "AllCops" => {
-              "Exclude" => []
-            }
-          }
-          repo_config = double("RepoConfig", for: config)
+          config = stub_ruby_config("AllCops" => { "Exclude" => [] })
           file = double("CommitFile", filename: "app.rb")
-          linter = build_linter(repo_config: repo_config)
+          linter = build_linter(config: config)
 
           expect(linter.file_included?(file)).to eq true
         end
@@ -703,10 +697,15 @@ describe Linter::Ruby do
 
     private
 
-    def violations_in(content, config: nil, repository_owner_name: "joe")
-      repo_config = build_repo_config(config)
+    def violations_in(
+      content,
+      config: stub_ruby_config,
+      repository_owner_name: "joe"
+    )
+      hound_config = build_hound_config
       linter = build_linter(
-        repo_config: repo_config,
+        hound_config: hound_config,
+        config: config,
         repository_owner_name: repository_owner_name,
       )
 
@@ -716,19 +715,36 @@ describe Linter::Ruby do
         flat_map(&:messages)
     end
 
+    def violations_with_config(config = stub_ruby_config)
+      content = <<-CODE.strip_heredoc
+        { :foo => "hello world" }
+      CODE
+
+      violations_in(content, config: config)
+    end
+
     def build_linter(
-      repo_config: build_repo_config,
+      hound_config: build_hound_config,
+      config: stub_ruby_config,
       repository_owner_name: "not_thoughtbot"
     )
+      config
       Linter::Ruby.new(
-        repo_config: repo_config,
+        hound_config: hound_config,
         build: build(:build),
         repository_owner_name: repository_owner_name,
       )
     end
 
-    def build_repo_config(config = "")
-      double("RepoConfig", enabled_for?: true, for: config)
+    def stub_ruby_config(config = "config")
+      stubbed_ruby_config = double("RubyConfig", content: config)
+      allow(Config::Ruby).to receive(:new).and_return(stubbed_ruby_config)
+
+      stubbed_ruby_config
+    end
+
+    def build_hound_config
+      double("HoundConfig", enabled_for?: true, content: "")
     end
 
     def build_file(content)

app/models/config/base.rb
@@ -1,0 +1,75 @@
+module Config
+  class Base
+    pattr_initialize :hound_config, :linter_name
+
+    def content
+      @content ||= parse(load_content)
+    rescue JSON::ParserError, Psych::Exception => exception
+      raise_parse_error(exception.message)
+    end
+
+    def excluded_files
+      []
+    end
+
+    private
+
+    DEFAULT_CONTENT = "{}"
+
+    attr_implement :parse, [:file_content]
+
+    def ensure_correct_type(result)
+      if result.is_a? Hash
+        result
+      else
+        raise_type_error
+      end
+    end
+
+    def raise_type_error
+      raise_parse_error("`#{file_path}` must be a Hash")
+    end
+
+    def load_content
+      if file_path
+        if url?
+          fetch_url
+        else
+          commit.file_content(file_path)
+        end
+      else
+        DEFAULT_CONTENT
+      end
+    end
+
+    def fetch_url
+      response = Faraday.new.get(file_path)
+
+      if response.success?
+        response.body
+      else
+        raise_parse_error("#{response.status} #{response.body}")
+      end
+    end
+
+    def url?
+      URI::regexp(%w(http https)).match(file_path)
+    end
+
+    def raise_parse_error(message)
+      raise Config::ParserError.new(message, filename: file_path)
+    end
+
+    def file_path
+      linter_config["config_file"]
+    end
+
+    def linter_config
+      hound_config.content[linter_name]
+    end
+
+    def commit
+      hound_config.commit
+    end
+  end
+end

app/models/config/coffee_script.rb
@@ -1,0 +1,19 @@
+module Config
+  class CoffeeScript < Base
+    private
+
+    def parse(file_content)
+      result = Parser.json(file_content)
+
+      ensure_correct_type(result)
+    end
+
+    def linter_config
+      super || hound_config.content[alternate_linter_name]
+    end
+
+    def alternate_linter_name
+      linter_name.sub("_", "")
+    end
+  end
+end

app/models/config/go.rb
@@ -1,0 +1,9 @@
+module Config
+  class Go < Base
+    DEFAULT_CONFIG = ""
+
+    def content
+      DEFAULT_CONFIG
+    end
+  end
+end

app/models/config/haml.rb
@@ -1,0 +1,11 @@
+module Config
+  class Haml < Base
+    private
+
+    def parse(file_content)
+      result = Parser.yaml(file_content)
+
+      ensure_correct_type(result)
+    end
+  end
+end

commit 0f465f7d69920221d5f7daf346dc3393399476db

Support `inherit_from` from Rubocop
A regression was introduced in fec130b, where Hound didn't
respect the specified files to inherit its configuration from.

app/models/config/ruby.rb
@@ -4,7 +4,8 @@ module Config
       if legacy?
         hound_config.content
       else
-        super
+        result = super
+        parse_inherit_from(result)
       end
     end
 
@@ -14,6 +15,18 @@ module Config
       result = Parser.yaml(file_content)
 
       ensure_correct_type(result)
+    end
+
+    def parse_inherit_from(config)
+      inherit_from = Array(config.fetch("inherit_from", []))
+
+      inherited_config = inherit_from.reduce({}) do |result, ancestor_file_path|
+        raw_ancestor_config = commit.file_content(ancestor_file_path)
+        ancestor_config = parse(raw_ancestor_config)
+        result.merge(ancestor_config)
+      end
+
+      inherited_config.merge(config.except("inherit_from"))
     end
 
     def legacy?

spec/models/config/ruby_spec.rb
@@ -30,6 +30,40 @@ describe Config::Ruby do
     end
   end
 
+  context "when the configuration uses `inherit_from`" do
+    it "returns the merged configuration using `inherit_from`" do
+      rubocop = <<-EOS.strip_heredoc
+        inherit_from:
+          - config/base.yml
+          - config/overrides.yml
+        Style/Encoding:
+          Enabled: true
+      EOS
+      base = <<-EOS.strip_heredoc
+        LineLength:
+          Max: 40
+      EOS
+      overrides = <<-EOS.strip_heredoc
+        Style/HashSyntax:
+          EnforcedStyle: hash_rockets
+        Style/Encoding:
+          Enabled: false
+      EOS
+      commit = stubbed_commit(
+        "config/rubocop.yml" => rubocop,
+        "config/base.yml" => base,
+        "config/overrides.yml" => overrides,
+      )
+      config = build_config(commit)
+
+      expect(config.content).to eq(
+        "LineLength" => { "Max" => 40 },
+        "Style/HashSyntax" => { "EnforcedStyle" => "hash_rockets" },
+        "Style/Encoding" => { "Enabled" => true },
+      )
+    end
+  end
+
   context "when the given content is invalid" do
     context "when the result is not a hash" do
       it "raises a type exception" do