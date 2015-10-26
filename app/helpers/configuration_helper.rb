adf508da77eb83944ab93d0b96e3b809e12eb258
Teo Ljungberg <teo@teoljungberg.com>
2015-10-26 16:23:22 +0100

    Document how to configure EsLint
    - Drop `"config"` from the table of contents.
    - Use `config_url` for all configuration urls in `ConfigurationHelper`

--- app/helpers/configuration_helper.rb
+++ app/helpers/configuration_helper.rb
@@ -1,30 +1,34 @@
 module ConfigurationHelper
   def ruby_config_url
-    "https://raw.githubusercontent.com/thoughtbot/hound/master/config/style_guides/ruby.yml"
+    config_url("thoughtbot/hound", "config/style_guides/ruby.yml")
   end
 
   def coffeescript_config_url
-    "https://raw.githubusercontent.com/thoughtbot/hound/master/config/style_guides/coffeescript.json"
+    config_url("thoughtbot/hound", "config/style_guides/coffeescript.json")
   end
 
   def javascript_config_url
-    "https://raw.githubusercontent.com/thoughtbot/hound/master/config/style_guides/javascript.json"
+    config_url("thoughtbot/hound", "config/style_guides/javascript.json")
   end
 
   def javascript_ignore_url
-    "https://raw.githubusercontent.com/thoughtbot/hound/master/config/style_guides/.jshintignore"
+    config_url("thoughtbot/hound", "config/style_guides/.jshintignore")
   end
 
   def jscs_config_url
     config_url("thoughtbot/guides", "style/javascript/.jscsrc")
   end
 
+  def eslint_config_url
+    config_url("thoughtbot/hound-eslint", "config/.eslintrc")
+  end
+
   def scss_config_url
-    "https://raw.githubusercontent.com/thoughtbot/hound-scss/master/config/default.yml"
+    config_url("thoughtbot/hound-scss", "config/default.yml")
   end
 
   def haml_config_url
-    "https://raw.githubusercontent.com/thoughtbot/hound/master/config/style_guides/haml.yml"
+    config_url("thoughtbot/hound", "config/style_guides/haml.yml")
   end
 
   def swift_config_url
