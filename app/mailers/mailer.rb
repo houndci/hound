5164ed3d2d0e473f80e16c16c558cfd59b45e98a
Prem Sichanugrist <s@sikac.hu>
2015-12-04 15:56:09 -0500

    Move from address to default

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,11 +1,12 @@
 class Mailer < ActionMailer::Base
+  default from: "Hound <hound@thoughtbot.com>".freeze
+
   def repo_activation_notification(repo, username, email)
     @repo = repo
     @subscriber = repo.subscriber
     @username = username
 
     mail(
-      from: "hound@thoughtbot.com",
       to: email,
       subject:
         "[Hound] Hound is now enabled on #{repo.full_github_name} repository"
