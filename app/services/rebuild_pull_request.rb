2d969a588ff58e3a671532e1faa8bea2dfef2400
Chris & Stephania <thorn+stephania@thoughtbot.com>
2016-03-04 11:12:11 -0800

    Add service to run a rebuild
    * Add `RebuildPullRequest` to trigger a new build from an existing build
      for a repo and pull request number.
    
    https://trello.com/c/hVwHmtpP

---
+++ app/services/rebuild_pull_request.rb
@@ -0,0 +1,21 @@
+class RebuildPullRequest
+  def initialize(repo:, pull_request_number:)
+    @repo = repo
+    @pull_request_number = pull_request_number
+  end
+
+  def call
+    if latest_build.present?
+      SmallBuildJob.perform_later(latest_build.payload)
+    end
+  end
+
+  private
+
+  def latest_build
+    Build.
+      where(repo: @repo, pull_request_number: @pull_request_number).
+      order(created_at: :desc).
+      first
+  end
+end
