9c49ab43d888cb16ddf1b32c753842887596ead4
Gaulin, Brendan <bgaulin@constantcontact.com>
2016-03-25 16:20:50 -0400

    Support .eslint.js
    ESLint is a bit complicated, and one of its complications, is how it handles config files.
    
    In hound currently, we pass the config file to the queue and assume it is JSON.
    
    This change, simply passes through file content as well as the file name so that when eslint goes to run it will know what to do.
    
    This enables support for .eslint.js and .eslint.yaml, as well as .eslint.json and .eslint
    
    In the future, we should probably find a better way to pass around configs? but this should work for now.

--- app/models/config/eslint.rb
+++ app/models/config/eslint.rb
@@ -1,15 +1,17 @@
 module Config
   class Eslint < Base
-    def serialize(data = content)
-      Serializer.json(data)
+    def serialize(_data = nil)
+      Serializer.json(
+        raw_content: load_content,
+        file_name: file_path,
+        hound_config_eslint_version: 2
+      )
     end
 
     private
 
     def parse(file_content)
-      json_with_comments = JsonWithComments.new(file_content)
-      content_without_comments = json_with_comments.without_comments
-      Parser.yaml(content_without_comments)
+      raise "Please don't parse me"
     end
   end
 end
