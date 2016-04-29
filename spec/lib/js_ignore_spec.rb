fa8c286d8009f26eecc36359176f1a26c4d5a9dd
Greg Lazarev <greg@thoughtbot.com>
2016-04-29 10:20:52 -0700

    Lookup ignore files using the linter's name (#1118)
    * Lookup ignore files using the linter's name
    
    There was a bug where `JsIgnore` looked up the ignore file only for the
    `javascript` linter. It now takes in a linter name and uses that as the
    key to look up in `.hound.yml`.
    
    * Set MultilineMethodCallIndentation Ruby rule to be indented

--- spec/lib/js_ignore_spec.rb
+++ spec/lib/js_ignore_spec.rb
@@ -5,23 +5,31 @@ describe JsIgnore do
   describe "#file_included?" do
     context "file is in excluded file list" do
       it "returns false" do
-        jsignore = build_js_ignore(["foo.js"])
-        commit_file = double("CommitFile", filename: "foo.js")
+        jsignore = build_js_ignore("javascript", "foo/*")
+        commit_file = double("CommitFile", filename: "foo/bar.js")
 
         expect(jsignore.file_included?(commit_file)).to eq false
+      end
+
+      context "with a different linter" do
+        it "returns false" do
+          jsignore = build_js_ignore("eslint", "foo/*")
+          commit_file = instance_double("CommitFile", filename: "foo/bar.js")
+
+          expect(jsignore.file_included?(commit_file)).to be false
+        end
       end
     end
 
     context "file is not excluded" do
       it "returns true" do
-        jsignore = build_js_ignore(["foo.js"])
-        commit_file = double("CommitFile", filename: "bar.js")
+        jsignore = build_js_ignore("javascript", "foo/*")
+        commit_file = double("CommitFile", filename: "foo.js")
 
         expect(jsignore.file_included?(commit_file)).to eq true
       end
 
       it "matches a glob pattern" do
-        jsignore = build_js_ignore(["app/assets/javascripts/*.js", "vendor/*"])
         commit_file1 = double(
           "CommitFile",
           filename: "app/assets/javascripts/bar.js",
@@ -30,17 +38,34 @@ describe JsIgnore do
           "CommitFile",
           filename: "vendor/assets/javascripts/foo.js",
         )
+        ignore_file_content = <<~TEXT
+          app/assets/javascripts/*.js\n
+          vendor/*
+        TEXT
+        jsignore = build_js_ignore("javascript", ignore_file_content)
 
         expect(jsignore.file_included?(commit_file1)).to be false
         expect(jsignore.file_included?(commit_file2)).to be false
       end
     end
 
-    def build_js_ignore(paths)
-      jsignore = JsIgnore.new({}, ".jsignore")
-      allow(jsignore).to receive(:ignored_paths).and_return(paths)
+    def build_js_ignore(linter, content)
+      hound_config = build_hound_config(linter, ".jsignore", content)
 
-      jsignore
+      JsIgnore.new("javascript", hound_config, ".jsignore")
+    end
+
+    def build_hound_config(linter, ignore_filename, content)
+      config_content = {
+        linter => {
+          "ignore_file" => ignore_filename,
+        },
+      }
+      commit = instance_double("Commit")
+      allow(commit).to receive(:file_content).with(ignore_filename).
+        and_return(content)
+
+      instance_double("HoundConfig", content: config_content, commit: commit)
     end
   end
 end
