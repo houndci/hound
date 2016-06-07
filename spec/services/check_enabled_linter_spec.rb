2461e8917818c8c4d39c71789a02633e06d7a5b7
Josh Morrow <jcmorrow@thoughtbot.com>
2016-06-07 15:01:06 -0700

    Rename `enabled_for?` to `linter_enabled?`

--- spec/services/check_enabled_linter_spec.rb
+++ spec/services/check_enabled_linter_spec.rb
@@ -5,7 +5,7 @@ describe CheckEnabledLinter do
   describe ".run" do
     context "when the hound config is enabled for the given language" do
       it "returns true" do
-        hound_config = double("HoundConfig", enabled_for?: true)
+        hound_config = double("HoundConfig", linter_enabled?: true)
         config = double(
           "Config",
           linter_names: ["ruby"],
@@ -20,7 +20,7 @@ describe CheckEnabledLinter do
 
     context "when the hound config is disabled for the given language" do
       it "returns false" do
-        hound_config = double("HoundConfig", enabled_for?: false)
+        hound_config = double("HoundConfig", linter_enabled?: false)
         config = double(
           "Config",
           linter_names: ["ruby"],
