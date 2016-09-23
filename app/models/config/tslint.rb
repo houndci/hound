e7045776ff7b13a75084c14e8fb7cee8d9f42175
Juan Ignacio Donoso <jidonoso@gmail.com>
2016-09-23 18:08:08 -0400

    Owner-level Tslint configuration

--- app/models/config/tslint.rb
+++ app/models/config/tslint.rb
@@ -1,5 +1,14 @@
 module Config
   class Tslint < Base
+    def initialize(hound_config, owner: nil)
+      super(hound_config)
+      @owner = owner
+    end
+
+    def content
+      owner_config_content.deep_merge(super)
+    end
+
     def serialize(data = content)
       Serializer.json(data)
     end
@@ -10,6 +19,18 @@ module Config
       json_with_comments = JsonWithComments.new(file_content)
       content_without_comments = json_with_comments.without_comments
       Parser.yaml(content_without_comments)
+    end
+
+    def owner_config_content
+      if @owner.present?
+        Config::TsLint.new(owner_hound_config).content
+      else
+        {}
+      end
+    end
+
+    def owner_hound_config
+      BuildOwnerHoundConfig.run(@owner)
     end
   end
 end
--- app/models/linter/tslint.rb
+++ app/models/linter/tslint.rb
@@ -2,4 +2,10 @@ module Linter
   class Tslint < Base
     FILE_REGEXP = /.+\.ts\z/
   end
+
+  private
+
+  def config
+    Config::TsLint.new(hound_config, owner: build.repo.owner)
+  end
 end
