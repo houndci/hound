d281332eff0e1daafb25e85334a0dd20009218e0
Rob Whittaker <cyclistsdismount@gmail.com>
2016-11-08 17:36:38 +0000

    Set the React variant in Production
    Previously, React was being rendered in an un-minified fashion in the
    Production environment, which was causing potential performance issues.
    Updated the Production configuration to use the Production variant of
    the React library.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,8 @@ Houndapp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.react.variant = :production
 end
 
 Rack::Timeout.timeout = (ENV["RACK_TIMEOUT"] || 20).to_i
