ce3343c4124cbcda1bab4aefd65649232be4e665
Greg Lazarev <greg@thoughtbot.com>
2018-03-23 09:52:47 -0700

    Show linter versions in the configuration docs
    Parsing most linter versions from `Gemfile.lock` and `yarn.lock`.
    
    Golint has no semver-like version, so we don't display any version.
    Credo, Flake8 and SwiftLint are hard-coded for now, because they don't
    often change.
    
    Re-ordered the linters on the configuration doc page for a better flow.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   include HighVoltage::StaticPage
 
   skip_before_action :authenticate
+
+  def configuration
+    @versions = LinterVersion.all
+  end
 end
