b25c54ed857315e16d64f4f508a87b60fe3a17e5
Scott Albertson <ascottalbertson@gmail.com>
2018-03-22 22:12:03 -0700

    Handle nil options when resolving config conflicts
    * Avoid `NoMethodError: undefined method `[]' for nil:NilClass`
    * Skip missing config options due to bad formatting

--- spec/services/resolve_config_conflicts_spec.rb
+++ spec/services/resolve_config_conflicts_spec.rb
@@ -23,5 +23,16 @@ describe ResolveConfigConflicts do
         expect(resolved_config["sass_lint"]).to eq("enabled" => true)
       end
     end
+
+    context "given nil config options" do
+      it "skips nil options" do
+        config = { "sass_lint" => nil, "eslint" => { "enabled" => true } }
+
+        resolved_config = ResolveConfigConflicts.call(config)
+
+        expect(resolved_config["jshint"]).to eq("enabled" => false)
+        expect(resolved_config["eslint"]).to eq("enabled" => true)
+      end
+    end
   end
 end
