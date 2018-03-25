b14150990dc452076ab31b59cc73b43c84c0abf0
Greg Lazarev <greg.lazarev@gmail.com>
2018-03-24 21:52:52 -0700

    Update user's email on login

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,16 +4,12 @@ describe SessionsController do
   describe "#create" do
     context "with valid new user" do
       it "creates new user" do
-        github_api = instance_double(
-          "GitHubApi",
-          scopes: "public_repo,user:email",
-        )
         request.env["omniauth.auth"] = stub_oauth(
           username: "jimtom",
           email: "jimtom@example.com",
           token: "letmein",
         )
-        allow(GitHubApi).to receive(:new).and_return(github_api)
+        allow(GitHubApi).to receive(:new).and_return(stub_github_api)
 
         expect { post :create }.to change { User.count }.by(1)
         user = User.last
@@ -34,5 +30,27 @@ describe SessionsController do
         expect(User.count).to be_zero
       end
     end
+
+    context "with existing user" do
+      it "updates the token and email" do
+        user = create(:user, username: "jim", email: "j@foo.com", token: "bar")
+        request.env["omniauth.auth"] = stub_oauth(
+          username: user.username,
+          email: "jim@example.com",
+          token: "letmein",
+        )
+        allow(GitHubApi).to receive(:new).and_return(stub_github_api)
+
+        post :create
+
+        user.reload
+        expect(user.email).to eq "jim@example.com"
+        expect(user.token).to eq "letmein"
+      end
+    end
+  end
+
+  def stub_github_api
+    instance_double("GitHubApi", scopes: "public_repo,user:email")
   end
 end
