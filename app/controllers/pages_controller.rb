ded9b66d47893f4fdd1730e193d3919d21061a1a
Greg Lazarev <greg.lazarev@gmail.com>
2018-03-26 09:34:42 -0700

    Revert "Show linter versions in the configuration docs" (#1508)
    This reverts commit c2d8c7ad9b06ef8e43072fe73be58d085c8ed12d.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,4 @@ class PagesController < ApplicationController
   include HighVoltage::StaticPage
 
   skip_before_action :authenticate
-
-  def configuration
-    @versions = LinterVersion.all
-  end
 end
