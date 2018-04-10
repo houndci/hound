b9a51adc97ec9f6fd0c33a64638220d4065c9e4e
Scott Albertson <ascottalbertson@gmail.com>
2018-04-10 10:32:22 -0700

    Verify GitHub webhook requests

--- app/controllers/deleted_subscriptions_controller.rb
+++ app/controllers/deleted_subscriptions_controller.rb
@@ -2,8 +2,18 @@ class DeletedSubscriptionsController < ApplicationController
   skip_before_action :authenticate, only: :create
 
   def create
-    DeleteSubscriptions.call(params)
+    payload = request.body.read
+    signature = request.env["HTTP_STRIPE_SIGNATURE"]
 
+    event = Stripe::Webhook.construct_event(
+      payload,
+      signature,
+      ENV.fetch("GITHUB_WEBHOOK_SECRET"),
+    )
+  rescue JSON::ParserError, Stripe::SignatureVerificationError
+    head 400
+  else
+    DeleteSubscriptions.call(params)
     head :ok
   end
 end
