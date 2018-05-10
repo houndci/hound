abe23ff4a3a731a14b0d051a4ed5b101056b432e
Greg Lazarev <greg.lazarev@gmail.com>
2018-05-09 17:16:38 -0700

    Remove user from repo if status can't be set

--- app/models/commit_status.rb
+++ app/models/commit_status.rb
@@ -7,15 +7,11 @@ class CommitStatus
 
   def set_pending
     github.create_pending_status(repo_name, sha, I18n.t(:pending_status))
-  rescue Octokit::NotFound
-    # noop
   end
 
   def set_success(violation_count)
     message = I18n.t(:complete_status, count: violation_count)
     github.create_success_status(repo_name, sha, message)
-  rescue Octokit::NotFound
-    # noop
   end
 
   def set_failure(violation_count)
@@ -36,8 +32,6 @@ class CommitStatus
 
   def create_error_status(repo_name, sha, message, configuration_url = nil)
     github.create_error_status(repo_name, sha, message, configuration_url)
-  rescue Octokit::NotFound
-    # noop
   end
 
   attr_reader :repo_name, :sha, :token
