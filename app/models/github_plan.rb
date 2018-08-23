15b621992ff09396b85e025b49cab50e14305fac
Scott Albertson <ascottalbertson@gmail.com>
2018-08-22 21:22:16 -0700

    Temporarily use staging Marketplace plans

--- app/models/github_plan.rb
+++ app/models/github_plan.rb
@@ -1,25 +1,50 @@
 class GitHubPlan < Plan
+  # PLANS = [
+  #   {
+  #     id: 1061,
+  #     price: 0,
+  #     range: 0..0,
+  #     title: "Hound",
+  #     slug: "MDIyOk1hcmtldHBsYWNlTGlzdGluZ1BsYW4xMDYx"
+  #   },
+  #   {
+  #     id: 1062,
+  #     price: 49,
+  #     range: 1..4,
+  #     title: "Chihuahua",
+  #     slug: "MDIyOk1hcmtldHBsYWNlTGlzdGluZ1BsYW4xMDYy"
+  #   },
+  #   {
+  #     id: 1063,
+  #     price: 149,
+  #     range: 5..20,
+  #     title: "Octodog",
+  #     slug: "MDIyOk1hcmtldHBsYWNlTGlzdGluZ1BsYW4xMDYz"
+  #   },
+  # ].freeze
+
+  # staging
   PLANS = [
     {
-      id: 1061,
+      id: 1501,
       price: 0,
       range: 0..0,
       title: "Hound",
-      slug: "MDIyOk1hcmtldHBsYWNlTGlzdGluZ1BsYW4xMDYx"
+      slug: "MDIyOk1hcmtldHBsYWNlTGlzdGluZ1BsYW4xNTAx"
     },
     {
-      id: 1062,
+      id: 1502,
       price: 49,
       range: 1..4,
       title: "Chihuahua",
-      slug: "MDIyOk1hcmtldHBsYWNlTGlzdGluZ1BsYW4xMDYy"
+      slug: "MDIyOk1hcmtldHBsYWNlTGlzdGluZ1BsYW4xNTAy"
     },
     {
-      id: 1063,
+      id: 1503,
       price: 149,
       range: 5..20,
       title: "Octodog",
-      slug: "MDIyOk1hcmtldHBsYWNlTGlzdGluZ1BsYW4xMDYz"
+      slug: "MDIyOk1hcmtldHBsYWNlTGlzdGluZ1BsYW4xNTAz"
     },
   ].freeze
 
