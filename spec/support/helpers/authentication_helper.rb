d35aa9a87b8d4389984b6d1b5b58f2d9c12d4ffa
Scott Albertson <ascottalbertson@gmail.com>
2018-09-12 19:41:27 -0700

    Stub User.find_by to stub #current_user

--- spec/support/helpers/authentication_helper.rb
+++ spec/support/helpers/authentication_helper.rb
@@ -6,16 +6,9 @@ module AuthenticationHelper
 
   def sign_in_as(user, token = "letmein")
     stub_oauth(username: user.username, email: user.email, token: token)
+    allow(User).to receive(:find_by).and_return(user)
+
     visit root_path
     find("[role=navigation]").click_on I18n.t("authenticate")
-    ensure_repos_load_via_ajax
-  end
-
-  private
-
-  def ensure_repos_load_via_ajax
-    if Capybara.current_driver != :rack_test
-      expect(page).to have_css(".organizations")
-    end
   end
 end
