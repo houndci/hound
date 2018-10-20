e58e0437a5a461d7f2ad802099f866fbaba2807a
Greg Lazarev <greg.lazarev@gmail.com>
2018-10-20 10:15:47 -0700

    Rename CoffeeScript to CoffeeLint

--- app/models/linter/coffeelint.rb
+++ app/models/linter/coffeelint.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module Linter
-  class CoffeeScript < Base
+  class Coffeelint < Base
     FILE_REGEXP = /.+\.coffee(\.js)?(\.erb)?\z/
 
     private
 
-    def job_name
-      "CoffeelintReviewJob"
+    def job_class
+      LintersJob
     end
   end
 end
