f44a50dbf15a45a7147980f205e4e55df1c0e8ea
Scott Albertson <ascottalbertson@gmail.com>
2018-11-14 10:47:19 -0800

    Set up A/B testing for landing page (#1635)
    * Use split to run and monitor A/B tests

---
+++ config/initializers/split.rb
@@ -0,0 +1,9 @@
+Split::Dashboard.use(Rack::Auth::Basic) do |username, password|
+  ActiveSupport::SecurityUtils.secure_compare(
+    ::Digest::SHA256.hexdigest(username),
+    ::Digest::SHA256.hexdigest(ENV.fetch("SPLIT_USERNAME")),
+  ) & ActiveSupport::SecurityUtils.secure_compare(
+    ::Digest::SHA256.hexdigest(password),
+    ::Digest::SHA256.hexdigest(ENV.fetch("SPLIT_PASSWORD")),
+  )
+end
