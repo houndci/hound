d7ec018708503d9e5ed65da861d9e29dbb9b7c92
Scott Albertson <ascottalbertson@gmail.com>
2019-01-15 21:31:39 -0800

    Rename unused block param

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 require "sidekiq/web"
 
-Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+Sidekiq::Web.use Rack::Auth::Basic do |_, password|
   ActiveSupport::SecurityUtils.secure_compare(
     ::Digest::SHA256.hexdigest(password),
     ::Digest::SHA256.hexdigest(ENV["QUEUE_ADMIN_PASSWORD"]),
