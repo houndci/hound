abf5a4ebf27a7282ae6d50b069b354f4eaf47476
Greg <greg.lazarev@gmail.com>
2019-09-09 13:08:16 -0700

    Configure OmniAuth to get the GitHub user's email
    GitHub Apps don't use scopes, but OmniAuth GitHub library still needs
    this option, to know to hit the GitHub API to fetch the email for the
    user that is logging in.

--- config/initializers/omni_auth.rb
+++ config/initializers/omni_auth.rb
@@ -1,5 +1,10 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, Hound::GITHUB_CLIENT_ID, Hound::GITHUB_CLIENT_SECRET
+  provider(
+    :github,
+    Hound::GITHUB_CLIENT_ID,
+    Hound::GITHUB_CLIENT_SECRET,
+    scope: "user:email",
+  )
 end
