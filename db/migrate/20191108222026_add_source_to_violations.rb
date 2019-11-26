7828c129b23f36dfe4f93f8e7a8fb8360189683b
Scott Albertson <ascottalbertson@gmail.com>
2019-11-25 17:50:45 -0800

    Remove concurrent migration option

--- db/migrate/20191108222026_add_source_to_violations.rb
+++ db/migrate/20191108222026_add_source_to_violations.rb
@@ -1,7 +1,5 @@
 class AddSourceToViolations < ActiveRecord::Migration[5.1]
-  disable_ddl_transaction!
-
   def change
-    add_column :violations, :source, :string, algorithm: :concurrently
+    add_column :violations, :source, :string
   end
 end
