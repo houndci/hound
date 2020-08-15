f067baaa5ce2854b419ab123c38abb34c4c3f939
Tyson Gach <tyson@tysongach.com>
2020-08-15 11:20:33 -0400

    Rejigger home page markup and styles
    - Organize styles using ITCSS (inverted triangle CSS) and use
      class namespaces (i.e. `c-` for components, `o-` for objects)
    - Detach styles from being named after the ‘home’ page - making them
      more reusable and descriptive of their purpose
    - Leverage CSS Custom Properties for better color management and
      component variants
        - This also opens the door for spinning-up themed landing pages,
          where the design can be unique to linter, for example
    - New styles do not need the Bourbon and Neat Sass libraries, so we
      could potentially drop these dependencies
    - Add and use the active_link_to gem which improves the accessibility of
      navigation links by outputting the `aria-current` attribute on the
      current page link
    - Use Rails Internationalization
    - Use WebP images (progressively-enhanced)
    - Improve accessibility with better heading hierarchy, added image alt
      text, and labeling page landmarks
    - Improve the Pricing section design

--- spec/support/helpers/authentication_helper.rb
+++ spec/support/helpers/authentication_helper.rb
@@ -8,7 +8,7 @@ module AuthenticationHelper
   def sign_in_as(user, token = "letmein")
     stub_oauth(username: user.username, email: user.email, token: token)
     visit root_path
-    find("[role=navigation]").click_on I18n.t("authenticate")
+    find(".c-app-nav").click_on I18n.t("authenticate")
     ensure_repos_load_via_ajax
   end
 
